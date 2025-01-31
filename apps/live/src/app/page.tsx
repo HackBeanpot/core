e8a9e746d70be5dc1ed6188211e6de692088fd66
achen32 <aretha.chen@gmail.com>
2025-01-30 20:42:15 -0500

    add footer

--- apps/live/src/app/page.tsx
+++ apps/live/src/app/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Landing from "./Sections/Landing";
 import EventSchedule from "./Sections/EventSchedule";
+import Footer from "./lib/Components/Footer";
 
 export default function Page(): JSX.Element {
   return (
     <main className="flex flex-col items-center">
       <Landing />
       <EventSchedule />
+      <Footer />
     </main>
   );
 }
