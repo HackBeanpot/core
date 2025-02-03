b26ffc88d070075bd8610ae6cb6bc50b5e3d0094
Yumiko Chow <75456756+yumi520@users.noreply.github.com>
2025-02-03 18:38:50 -0500

    Yc/resources - Resources Page for Live (#58)
    * added resources section with cards
    
    * content height for background
    
    * cleaned up code
    
    * better background on desktop
    
    ---------
    
    Co-authored-by: Lucas Dunker <ldunker2003@gmail.com>

--- apps/live/src/app/page.tsx
+++ apps/live/src/app/page.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import Landing from "./Sections/Landing";
 import EventSchedule from "./Sections/EventSchedule";
+import Resources from "./Sections/Resources";
 import NavBar from "./lib/Components/NavBar";
 import Footer from "./lib/Components/Footer";
-
+        
 export default function Page(): JSX.Element {
   return (
     <main className="flex flex-col items-center">
       <NavBar />
       <Landing />
       <EventSchedule />
+      <Resources />
       <Footer />
+
     </main>
   );
 }
