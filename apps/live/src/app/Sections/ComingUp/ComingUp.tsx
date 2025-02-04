06c9af8668e09d7cc17c47666abb78864c8653f6
Yumiko (Yumi) Chow <75456756+yumi520@users.noreply.github.com>
2025-02-03 23:20:53 -0500

    airtable api for coming up section and fixed resources background

--- apps/live/src/app/Sections/ComingUp/ComingUp.tsx
+++ apps/live/src/app/Sections/ComingUp/ComingUp.tsx
@@ -1,11 +1,10 @@
-"use client"
+"use client";
 
 import React, { useEffect, useState } from "react";
 import ComingUpEvent from "./ComingUpEvent";
 import { Section } from "@repo/ui";
 import ComingUpBackground from "../../lib/Assets/SVG/ComingUpBackground";
 import removeHoursFromDate from "@util/functions/removeHoursfromDate";
-
 
 export type AirtableRecord = {
   id: string;
@@ -29,132 +28,126 @@ type EventByDate = {
   [date: string]: AirtableRecord[];
 };
 
-const toTime = (time: string, showAmPm: boolean): string => {
-  const date = new Date(time);
+// const toTime = (time: string, showAmPm: boolean): string => {
+//   const date = new Date(time);
 
-  if (showAmPm) {
-    return date.toLocaleTimeString("en-us", {
-      hour: "numeric",
-      minute: "2-digit",
-      hour12: true,
-    });
-  }
+//   if (showAmPm) {
+//     return date.toLocaleTimeString("en-us", {
+//       hour: "numeric",
+//       minute: "2-digit",
+//       hour12: true,
+//     });
+//   }
 
-  let hours = date.getHours();
-  const minutes = date.getMinutes().toString().padStart(2, "0");
+//   let hours = date.getHours();
+//   const minutes = date.getMinutes().toString().padStart(2, "0");
 
-  // Convert to 12-hour format
-  hours = hours % 12;
-  hours = hours ? hours : 12; // the hour '0' should be '12'
+//   // Convert to 12-hour format
+//   hours = hours % 12;
+//   hours = hours ? hours : 12; // the hour '0' should be '12'
 
-  // Combine hours and minutes
-  const timeString = `${hours}:${minutes}`;
+//   // Combine hours and minutes
+//   const timeString = `${hours}:${minutes}`;
 
-  return timeString;
-};
+//   return timeString;
+// };
 
-const isHappeningNow = (startTime: string, endTime: string): boolean => {
-  const now = new Date();
-  const startDate = new Date(startTime);
-  const endDate = new Date(endTime);
-  return startDate <= now && now <= endDate;
-};
-
+// const isHappeningNow = (startTime: string, endTime: string): boolean => {
+//   const now = new Date();
+//   const startDate = new Date(startTime);
+//   const endDate = new Date(endTime);
+//   return startDate <= now && now <= endDate;
+// };
 
 const ComingUpContent = () => {
   const [data, setData] = useState<AirtableData | null>(null);
-  const [loading, setLoading] = useState<boolean>(false);
-  
-    async function getScheduleData() {
-      setLoading(true);
-      const res = await fetch("/api/airtable");
-  
-      const status = res.status;
-  
-      if (status == 200) {
-        setData(await res.json());
-      }
-      setLoading(false);
+  //const [loading, setLoading] = useState<boolean>(false);
+
+  async function getScheduleData() {
+    //setLoading(true);
+    const res = await fetch("/api/airtable");
+
+    const status = res.status;
+
+    if (status == 200) {
+      setData(await res.json());
     }
-  
-    useEffect(() => {
-      getScheduleData();
-    }, []);
+    //setLoading(false);
+  }
 
-    const eventsByDate =
-        data?.records.reduce((acc, record) => {
-          const eventDate = new Date(record.fields.start_time);
-          const uniqueEventDate = removeHoursFromDate(eventDate);
-          if (!acc[uniqueEventDate.toISOString()]) {
-            acc[uniqueEventDate.toISOString()] = [];
-          }
-          acc[uniqueEventDate.toISOString()].push(record);
-          return acc;
-        }, {} as EventByDate) || null;
-    
-      const datesSorted = Object.keys(eventsByDate || {}).sort((a, b) => {
-        const dateA = new Date(a);
-        const dateB = new Date(b);
-        return dateA.getTime() - dateB.getTime();
-      });
+  useEffect(() => {
+    getScheduleData();
+  }, []);
 
-      const upcomingEvents = datesSorted
-      .flatMap((date) => eventsByDate?.[date] || []) // Flatten all events into a single array
-      .filter((event) => {
-        const now = new Date();
-        const startTime = new Date(event.fields.start_time);
-        const endTime = new Date(event.fields.end_time);
-        return startTime >= now || (startTime <= now && now <= endTime); // Include events that are happening now
-      })
-      .slice(0, 3);
+  const eventsByDate =
+    data?.records.reduce((acc, record) => {
+      const eventDate = new Date(record.fields.start_time);
+      const uniqueEventDate = removeHoursFromDate(eventDate);
+      if (!acc[uniqueEventDate.toISOString()]) {
+        acc[uniqueEventDate.toISOString()] = [];
+      }
+      acc[uniqueEventDate.toISOString()].push(record);
+      return acc;
+    }, {} as EventByDate) || null;
 
-      console.log(upcomingEvents)
-    
+  const datesSorted = Object.keys(eventsByDate || {}).sort((a, b) => {
+    const dateA = new Date(a);
+    const dateB = new Date(b);
+    return dateA.getTime() - dateB.getTime();
+  });
+
+  const upcomingEvents = datesSorted
+    .flatMap((date) => eventsByDate?.[date] || []) // Flatten all events into a single array
+    .filter((event) => {
+      const now = new Date();
+      const startTime = new Date(event.fields.start_time);
+      const endTime = new Date(event.fields.end_time);
+      return startTime >= now || (startTime <= now && now <= endTime); // Include events that are happening now
+    })
+    .slice(0, 3);
+
   return (
     <div>
-      <h1 className="flex justify-center text-[clamp(3rem,6vw,7rem)] text-[#045954] font-Wilden pt-[3rem]">COMING UP!</h1>
-      <div className={`flex items-center ${upcomingEvents.length == 0 ? 'justify-center' : 'justify-between'} w-full h-full content-center pr-[10rem] pl-[10rem] pt-[5rem]`}>
-        {(upcomingEvents.length == 0 || upcomingEvents == undefined) ? 
-        <div>
-          {/* <h1 className="flex justify-center items-center text-black text-2xl mb-1">NO EVENTS</h1>  */}
-          <ComingUpEvent title="NO EVENTS" subtitle="Check back later!" deadline="" description=""/>
-        </div>
-          :
-          
+      <h1 className="flex justify-center text-[clamp(3rem,6vw,7rem)] text-[#045954] font-Wilden pt-[3rem]">
+        COMING UP!
+      </h1>
+      <div
+        className={`flex items-center ${upcomingEvents.length == 0 ? "justify-center" : "justify-between"} w-full h-full content-center pr-[10rem] pl-[10rem] pt-[5rem]`}
+      >
+        {upcomingEvents.length == 0 || upcomingEvents == undefined ? (
+          <div>
+            <ComingUpEvent
+              title="NO EVENTS"
+              subtitle="Check back later!"
+              deadline=""
+              description=""
+            />
+          </div>
+        ) : (
           upcomingEvents.map((event) => (
-          <ComingUpEvent
-            key={event.id}
-            title={'NOW'}
-            subtitle={event.fields.eventName}
-            deadline={event.fields.end_time}
-            description={event.fields.description}
-            // key={idx}
-            // title={'NOW'}
-            // subtitle={'subtitle'}
-            // deadline={'deadline'}
-            // description={'description'}
-          />
-        ))
-        }
+            <ComingUpEvent
+              key={event.id}
+              title={"NOW"}
+              subtitle={event.fields.eventName}
+              deadline={event.fields.end_time}
+              description={event.fields.description}
+              // key={idx}
+              // title={'NOW'}
+              // subtitle={'subtitle'}
+              // deadline={'deadline'}
+              // description={'description'}
+            />
+          ))
+        )}
       </div>
     </div>
   );
 };
 
 const ComingUp = () => {
-  // API call to fetch data
-
-  // Filter 3 most recent events
-
-  // Decide if event is happening now
-  // return (
-  //     <ComingUpEvent />
-  // )
-
   return (
     <Section
       name={"calendar"}
-      //   foreground={foreground}
       background={<ComingUpBackground />}
       content={<ComingUpContent />}
       height={100}
--- apps/live/src/app/lib/Assets/SVG/ResourcesExternalLink.tsx
+++ apps/live/src/app/lib/Assets/SVG/ResourcesExternalLink.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 
 export default function ResourcesExternalLink() {
-    return (
-        <svg width="42" height="42" viewBox="0 0 42 42" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M21 21L36.75 5.25M36.75 5.25H28M36.75 5.25V14M36.75 24.5V33.25C36.75 34.1783 36.3813 35.0685 35.7249 35.7249C35.0685 36.3813 34.1783 36.75 33.25 36.75H8.75C7.82174 36.75 6.9315 36.3813 6.27513 35.7249C5.61875 35.0685 5.25 34.1783 5.25 33.25V8.75C5.25 7.82174 5.61875 6.9315 6.27513 6.27513C6.9315 5.61875 7.82174 5.25 8.75 5.25H17.5" stroke="#90B3A2" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"/>
-        </svg>
-
-    )
+  return (
+    <svg
+      width="42"
+      height="42"
+      viewBox="0 0 42 42"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M21 21L36.75 5.25M36.75 5.25H28M36.75 5.25V14M36.75 24.5V33.25C36.75 34.1783 36.3813 35.0685 35.7249 35.7249C35.0685 36.3813 34.1783 36.75 33.25 36.75H8.75C7.82174 36.75 6.9315 36.3813 6.27513 35.7249C5.61875 35.0685 5.25 34.1783 5.25 33.25V8.75C5.25 7.82174 5.61875 6.9315 6.27513 6.27513C6.9315 5.61875 7.82174 5.25 8.75 5.25H17.5"
+        stroke="#90B3A2"
+        strokeWidth="3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
 }
