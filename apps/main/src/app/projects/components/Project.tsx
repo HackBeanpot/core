68bd09b9f6ea05a6d40a5246d3abb86f6b0658bd
Zahra Wibisana <wibisana.z@northeastern.edu>
2025-02-04 18:27:01 -0500

    fixed spacing on projects

--- apps/main/src/app/projects/components/Project.tsx
+++ apps/main/src/app/projects/components/Project.tsx
@@ -18,57 +18,30 @@ function Project({
 }: ProjectProps) {
   return (
     <div
-      className="flex flex-row pt-[3rem] pr-[7rem] mb-[2rem] bg-no-repeat justify-center bg-cover bg-center"
+      className="flex flex-row pt-[3rem] pb-[3rem] pr-[7rem] bg-no-repeat justify-center  bg-center max-h-[540.75px] overflow-hidden"
       style={{
         backgroundImage: "url('/stamp.svg')",
       }}
     >
-      <div className="mr-10">
-        <h2 className="text-2xl text-right -rotate-90 font-bold origin-right">
+      <div className="">
+        <h2 className="text-2xl text-right font-bold -rotate-90 origin-right font-Wilden-Regular">
           {projectName}
         </h2>
       </div>
-      <div className="max-w-40 mb-5">
+      <div className="max-w-40 mb-5 ml-10">
         <Image alt="" src={projectImage} width={200} height={200} />
-        <h2 className="font-semibold text-xl text-[#5D5D5D] mb-5 mt-5">
+        <h2 className="font-semibold text-xl text-[#5D5D5D] mt-5 font-GT-Walsheim-Regular">
           {award}
         </h2>
-        <p className="text-[#918E89] mb-5 mt-5">{members}</p>
-        <p className="text-[#333333]">{description}</p>
+        <p className="text-[#918E89] mb-2 mt-5 text-l font-GT-Walsheim-Regular">
+          {members}
+        </p>
+        <p className="text-[#333333] text-l font-GT-Walsheim-Regular hyphens-auto overflow-hidden text-ellipsis">
+          {description}
+        </p>
       </div>
     </div>
   );
 }
-
-// const Project: React.FC<ProjectProps> = ({
-//   projectImage,
-//   projectName,
-//   award,
-//   members,
-//   description,
-// }) => {
-//   return (
-//     <div
-//       className="flex flex-row pt-[3rem] pr-[7rem] mb-[2rem] bg-no-repeat justify-center bg-cover bg-center"
-//       style={{
-//         backgroundImage: "url('/stamp.svg')",
-//       }}
-//     >
-//       <div className="mr-10">
-//         <h2 className="text-2xl text-right -rotate-90 font-bold origin-right">
-//           {projectName}
-//         </h2>
-//       </div>
-//       <div className="max-w-40 mb-5">
-//         <Image alt="" src={projectImage} width={200} height={200} />
-//         <h2 className="font-semibold text-xl text-[#5D5D5D] mb-5 mt-5">
-//           {award}
-//         </h2>
-//         <p className="text-[#918E89] mb-5 mt-5">{members}</p>
-//         <p className="text-[#333333]">{description}</p>
-//       </div>
-//     </div>
-//   );
-// };
 
 export default Project;
--- apps/main/src/app/projects/content.tsx
+++ apps/main/src/app/projects/content.tsx
@@ -5,11 +5,11 @@ const ProjectsContent = () => {
   return (
     <div className="mt-[5%] mr-[5%] ml-[5%]">
       <div className="">
-        <h1 className="font-bold text-5xl text-center">
-          LAST YEAR&apos;S PROJECT
+        <h1 className="font-bold text-6xl text-center text-[#FFFFFF] font-Wilden-Regular">
+          LAST YEAR&apos;S PROJECTS
         </h1>
       </div>
-      <section className="flex flex-wrap mt-[5%] justify-evenly">
+      <section className="grid grid-cols-3 gap-2">
         <Project
           projectImage="/poqua.png"
           projectName="POQUA"
@@ -19,28 +19,28 @@ const ProjectsContent = () => {
         />
         <Project
           projectImage="/discountbytes.png"
-          projectName="POQUA"
+          projectName="DISCOUNTBYTES"
           award="Most Creative"
           members="Shashwath Sunkum, B D S Aritra, Samuel Steinmetz, Sahil Shah"
           description="A web application designed specifically for restaurants, allowing them to dynamically adjust their menu pricing based on real-time customer traffic."
         />
         <Project
           projectImage="/lastminute.png"
-          projectName="POQUA"
+          projectName="LAST MINUTE"
           award="Social Impact/Sustainability"
           members="Yujin Park, Justin Kim, Emily Ye, Oscar Fang"
           description="Last Minute allows users to upload pictures of their leftover food, which can then be picked up by anyone in the area who needs a meal at the very last minute."
         />
         <Project
           projectImage="/closet.png"
-          projectName="POQUA"
+          projectName="CLOSET COMPANION"
           award="Best Beginner"
           members="Krystal Qiao"
           description="Effortlessly curate outfits from your wardrobe, tailored to your unique taste. Say goodbye to fashion dilemmas and hello to  digital fashion solutions."
         />
         <Project
           projectImage="/maritime.png"
-          projectName="POQUA"
+          projectName="MARITIME"
           award="Most Technically Challenging"
           members="Kaylee Wu, Mouad Tiahi, Kenneth Chap, Borui Chen"
           description="Maritime is an iOS, Android, and web application that promotes recycling and mitigating waste, specifically in the world's oceans."
@@ -54,21 +54,21 @@ const ProjectsContent = () => {
         />
         <Project
           projectImage="/letslink.png"
-          projectName="POQUA"
+          projectName="LET'S LINK"
           award="(MLH) Best Use of MongoDB Atlas"
           members="Russell Leung, Aahil Nishad, Michael Mehall, Donny Le, Andrew Zhu"
           description="Finding times to meet can be hard. With Let's Link, you can connect your Google Calendar and we'll provide the most optimal times."
         />
         <Project
           projectImage="/perfectpantry.png"
-          projectName="POQUA"
+          projectName="PERFECT PANTRY"
           award="(MLH) Best Domain Name from GoDaddy Registry"
           members="Ayomide Addey, Arushi Aggarwal"
           description="Have you ever looked at an empty pantry and wondered what you could make with such few ingre-dients? The Perfect Pantry can help you find out!"
         />
         <Project
           projectImage="/findingfishe.png"
-          projectName="POQUA"
+          projectName="FINDING FISHE"
           award="(MLH) Best DEI Hack sponsored by Fidelity"
           members="Alina Gonzalez, Alex Chen, Spring Yan, Dao Ho"
           description="A web app designed to help small business owners by providing them with the ability to determine if receipts represent suspicious."
