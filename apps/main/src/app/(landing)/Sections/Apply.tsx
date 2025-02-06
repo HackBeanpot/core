f9689db5e31309a08d48e3f8ced6b42831d6ccdb
Yumiko (Yumi) Chow <75456756+yumi520@users.noreply.github.com>
2025-02-05 22:52:15 -0500

    made content responsive

--- apps/main/src/app/(landing)/Sections/Apply.tsx
+++ apps/main/src/app/(landing)/Sections/Apply.tsx
@@ -4,6 +4,9 @@ import React from "react";
 import Image from "next/image";
 import { Section } from "@repo/ui";
 import useIsMobile from "@util/hooks/useIsMobile";
+import ApplyLeftLog from "../../lib/Assets/SVG/ApplyAssets/ApplyLeftLog";
+import ApplyRightLog from "../../lib/Assets/SVG/ApplyAssets/ApplyRightLog";
+
 
 const Apply = () => {
   const isMobile = useIsMobile();
@@ -23,7 +26,7 @@ const Apply = () => {
     <div>
       {isMobile ? (
         <div className="relative flex flex-col top-10 justify-center text-center items-center">
-          <div className="relative m-auto w-3/6 flex justify-center text-center items-center text-2xl pb-3">
+          <div className="relative m-auto w-3/6 flex justify-center text-center items-center text-2xl pb-3 font-GT-Walsheim-Regular">
             <p>
               We’re going on a roadtrip! Reach new destinations at our
               hackathon. Applications close 12/3 @ 11:59 PM.
@@ -32,34 +35,22 @@ const Apply = () => {
 
           <button
             onClick={() => alert("apply not implemented yet")}
-            className="p-3 bg-[#84AF67] text-text-light rounded-full drop-shadow-md w-44 h-14 text-2xl"
+            className="p-3 bg-[#84AF67] text-text-light rounded-full drop-shadow-md w-44 h-14 text-2xl font-GT-Walsheim-Regular"
           >
             Apply here
           </button>
         </div>
       ) : (
-        <div>
-          <div className="relative top-28">
-            <Image
-              alt="LeftLogs"
-              src="/left_logs.svg"
-              width={300}
-              height={700}
-              className=""
-            />
-          </div>
+        <div className="h-full w-full">
+           <div className="relative top-20">
+            <ApplyLeftLog />
+           </div>
 
-          <div className="absolute top-32 right-0">
-            <Image
-              alt="RightLogs"
-              src="/right_logs.svg"
-              width={300}
-              height={700}
-              className=""
-            />
-          </div>
-          <div className="relative flex flex-col bottom-40 justify-center text-center items-center">
-            <div className="relative m-auto w-3/6 flex justify-center text-center items-center text-2xl pb-10">
+           <div className="absolute top-20 right-0">
+            <ApplyRightLog />
+           </div>
+          <div className="relative flex flex-col bottom-72 justify-center text-center items-center">
+            <div className="relative m-auto w-3/6 flex justify-center text-center items-center text-2xl pb-7 font-GT-Walsheim-Regular">
               <p>
                 We’re going on a roadtrip! Reach new destinations at our
                 hackathon. Applications close 12/3 @ 11:59 PM.
