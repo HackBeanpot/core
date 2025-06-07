3cc0a4a1954f182b9940d8736e6ab4b4e5b9c558
Khushi Khan <76977629+khushikhan0@users.noreply.github.com>
2025-06-06 21:53:51 -0400

    fixed background, bush, and log placement

--- apps/main/src/app/(landing)/Sections/Apply.tsx
+++ apps/main/src/app/(landing)/Sections/Apply.tsx
@@ -3,30 +3,47 @@
 import React from "react";
 import ApplyLeftLog from "../../lib/Assets/SVG/ApplyAssets/ApplyLeftLog";
 import ApplyRightLog from "../../lib/Assets/SVG/ApplyAssets/ApplyRightLog";
+import ApplyBackground from "../../lib/Assets/SVG/ApplyAssets/ApplyBackground";
+import ApplyLeftBush from "../../lib/Assets/SVG/ApplyAssets/ApplyLeftBush";
+import ApplyRightBush from "../../lib/Assets/SVG/ApplyAssets/ApplyRightBush";
 
 const Apply = () => {
   return (
     <>
-      <div className="w-full py-48 bg-[url(/apply_background.svg)] bg-cover bg-center flex flex-row justify-center items-center">
-        {/* so that when we're not in desktop, logs are no longer visible */}
-        <div className="invisible desktop:visible">
+      <div className="relative w-full mt-44 flex flex-col flex-none h-[559px]">
+        <div className="w-full h-full z-0">
+          <ApplyBackground height={555} preserveAspectRatio="none"/>
+        </div>
+
+        <div className="relative top-[-480px] z-10 flex justify-center items-center w-full h-full">
+          <div className="w-1/2 text-center">
+            <p className="text-[20px] tablet:desktop:text-2xl desktop:text-3xl font-GT-Walsheim-Regular">
+              Applications have not been released for HackBeanpot 2026. Check in with us again this fall!
+            </p>
+            <div className="pt-8">
+              <button
+                onClick={() => alert("Applications for HackBeanpot 2026 have not been released. Check in with us again this fall!")}
+                className="p-3 bg-[#84AF67] text-text-light rounded-full drop-shadow-md w-44 h-14 text-2xl"
+              >
+                Apply here
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div className="absolute top-[-60px] left-0 w-[40%] h-[120%] z-31 invisible desktop:visible">
+          <ApplyLeftBush className="w-full h-full" />
+        </div>
+        
+        <div className="absolute top-[-140px] -right-16 w-[40%] h-[120%] z-31 invisible desktop:visible">
+          <ApplyRightBush className="w-full h-full" />
+        </div> 
+
+        <div className="absolute top-[30px] left-0 w-[40%] h-[120%] z-31 invisible desktop:visible">
           <ApplyLeftLog />
         </div>
 
-        <div className="w-full h-full relative w-1/2 flex flex-col justify-center text-center items-center text-2xl tablet:desktop:text-3xl desktop:text-4xl pb-7 font-GT-Walsheim-Regular">
-          <p>
-            Applications have not been released for HackBeanpot 2026. Check in with us again this fall!
-          </p>
-          <div className="pt-10">
-            <button
-              onClick={() => alert("Applications for HackBeanpot 2026 have not been released. Check in with us again this fall!")}
-              className="p-3 bg-[#84AF67] text-text-light rounded-full drop-shadow-md w-44 h-14 text-2xl"
-            >
-              Apply here
-            </button>
-          </div>
-        </div>
-        <div className="invisible desktop:visible">
+        <div className="absolute top-[40px] -right-40 w-[40%] h-[120%] z-31 invisible desktop:visible">
           <ApplyRightLog />
         </div>
       </div>
