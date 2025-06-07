8ffc4e8bf90315abe072560ae503edefe2f213d4
Aditya <appathak46@gmail.com>
2025-06-07 10:01:28 -0400

    changes to about landing

--- apps/main/src/app/(landing)/Sections/About.tsx
+++ apps/main/src/app/(landing)/Sections/About.tsx
@@ -11,23 +11,25 @@ import Journal from "../../lib/Assets/SVG/AboutAssets/Journal";
 import Thumbtack1 from "../../lib/Assets/SVG/AboutAssets/Thumbtack1";
 import Thumbtack2 from "../../lib/Assets/SVG/AboutAssets/Thumbtack2";
 import Thumbtack3 from "../../lib/Assets/SVG/AboutAssets/Thumbtack3";
-
+import AboutParagraph from "../../lib/Assets/SVG/AboutAssets/AboutParagraph";
 export default function About(): React.ReactNode {
   return (
-    <div className="w-full h-[100vh] bg-[#F3E7D7] flex flex-col flex-none">
-      <div className="w-[100vw]">
-        <TopAboutBar />
-      </div>
-      <div className="w-[55vw]">
-        <Squiggle />
-      </div>
-      <div className="w-[100vw] mt-auto">
-        <BottomAboutBar />
-      </div>
+    <div className="w-full h-full min-h-screen bg-[#F3E7D7] flex flex-col relative">
+    <div className="w-full">
+      <TopAboutBar />
+        </div>
 
-      <div className="absolute w-[45vw] left-[56vw] transform">
-        <Journal />
-      </div>
+    <div className="w-full px-4 sm:px-12 md:px-24 lg:px-36 mt-4">
+    <Squiggle />
+    </div>
+
+    <div className="w-full mt-auto">
+    <BottomAboutBar />
+  </div>
+
+  <div className="absolute hidden md:block w-[45vw] left-[56vw]">
+    <Journal />
+  </div>
       {/* the journal parts*/}
       <div className="absolute left-[90%] mt-24">
         <Image alt="AboutPicture" src="/about_1.png" width={250} height={100} />
@@ -55,14 +57,7 @@ export default function About(): React.ReactNode {
         <div className="w-[50vw]">
           <StreetSign streetName={"ABOUT"} suffix="HBP" />
           <div>
-            <p className="font-GT-Walsheim-Regular mt-12">
-              We&apos;re a non-profit organization in the Boston area that
-              organizes an annual undergraduate hackathon. Our goal is to expand
-              and nurture the hacker culture that exists in Boston and the
-              surrounding areas. We connect students and other aspiring nerds to
-              their colleagues so that they can meet new people, learn new
-              things, and have a great time.
-            </p>
+          <AboutParagraph />
           </div>
           <button className="mt-12 bg-[#02877F] text-white py-3 px-7 rounded-[64px]">
             {"View Past Photos"}
