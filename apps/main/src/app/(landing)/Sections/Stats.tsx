f5c47845e95c3bd1ba09e46f1083b646ae195271
Yumiko (Yumi) Chow <75456756+yumi520@users.noreply.github.com>
2025-06-11 22:13:01 -0700

    added responsiveness to stats section

--- apps/main/src/app/(landing)/Sections/Stats.tsx
+++ apps/main/src/app/(landing)/Sections/Stats.tsx
@@ -4,17 +4,17 @@ import Box from "@repo/ui/Box";
 export default function Stats(): React.ReactNode {
   return (
     <div className="w-full -mt-64">
-      <div className="w-full bg-[#BEACD0] p-16 flex flex-row gap-16 justify-center content-center text-xl tablet:text-3xl">
+      <div className="w-full bg-[#BEACD0] p-16 flex flex-row flex-wrap gap-16 justify-center content-center text-xl tablet:text-3xl">
         <Box>
-          <p className="text-4xl tablet:text-8xl font-medium mb-2">{"10+"}</p>
-          <p>{"years of HackBeanpot"}</p>
+          <p className="text-4xl tablet:text-7xl font-medium mb-2">{"10+"}</p>
+          <p>{"years of HBP"}</p>
         </Box>
         <Box>
-          <p className="text-4xl tablet:text-8xl font-medium mb-2">{"10+"}</p>
+          <p className="text-4xl tablet:text-7xl font-medium mb-2">{"10+"}</p>
           <p>{"sponsors each year"}</p>
         </Box>
         <Box>
-          <p className="text-4xl tablet:text-8xl font-medium mb-2">{"800+"}</p>
+          <p className="text-4xl tablet:text-7xl font-medium mb-2">{"800+"}</p>
           <p>{"beans and counting"}</p>
         </Box>
       </div>
