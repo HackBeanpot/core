1fc80b1d8b575eef8707c3e60ed493d5add6d3c5
Khushi Khan <76977629+khushikhan0@users.noreply.github.com>
2025-06-13 19:32:33 -0400

    fixed svg stretch issue

--- apps/main/src/app/(landing)/Sections/Apply.tsx
+++ apps/main/src/app/(landing)/Sections/Apply.tsx
@@ -10,19 +10,24 @@ import ApplyRightBush from "../../lib/Assets/SVG/ApplyAssets/ApplyRightBush";
 const Apply = () => {
   return (
     <>
-      <div className="relative w-full mt-44 flex flex-col flex-none h-[559px]">
+      <div className="relative w-full h-[68vh] mt-44 sm:mt-36 mb-14 flex flex-col flex-none">
         <div className="w-full h-full z-0">
-          <ApplyBackground height={555} preserveAspectRatio="none"/>
+          <ApplyBackground preserveAspectRatio="xMidYMid slice" />
         </div>
 
-        <div className="relative top-[-480px] z-10 flex justify-center items-center w-full h-full">
-          <div className="w-1/2 text-center">
+        <div className="absolute inset-0 top-[-20%] z-10 flex justify-center items-center px-4 text-center">
+          <div className="max-w-xl">
             <p className="text-[20px] tablet:desktop:text-2xl desktop:text-3xl font-GT-Walsheim-Regular">
-              Applications have not been released for HackBeanpot 2026. Check in with us again this fall!
+              Applications have not been released for HackBeanpot 2026. Check in
+              with us again this fall!
             </p>
             <div className="pt-8">
               <button
-                onClick={() => alert("Applications for HackBeanpot 2026 have not been released. Check in with us again this fall!")}
+                onClick={() =>
+                  alert(
+                    "Applications for HackBeanpot 2026 have not been released. Check in with us again this fall!",
+                  )
+                }
                 className="p-3 bg-[#84AF67] text-text-light rounded-full drop-shadow-md w-44 h-14 text-2xl"
               >
                 Apply here
@@ -31,20 +36,20 @@ const Apply = () => {
           </div>
         </div>
 
-        <div className="absolute top-[-60px] left-0 w-[40%] h-[120%] z-31 invisible desktop:visible">
+        <div className="absolute top-[-5%] left-0 max-w-[120%] h-[130%] z-31 invisible desktop:visible">
           <ApplyLeftBush className="w-full h-full" />
         </div>
-        
-        <div className="absolute top-[-140px] -right-16 w-[40%] h-[120%] z-31 invisible desktop:visible">
-          <ApplyRightBush className="w-full h-full" />
-        </div> 
 
-        <div className="absolute top-[30px] left-0 w-[40%] h-[120%] z-31 invisible desktop:visible">
-          <ApplyLeftLog />
+        <div className="absolute top-[-15%] right-[-2%] max-w-[120%] h-[120%] z-31 invisible desktop:visible">
+          <ApplyRightBush className="w-full h-full" />
         </div>
 
-        <div className="absolute top-[40px] -right-40 w-[40%] h-[120%] z-31 invisible desktop:visible">
-          <ApplyRightLog />
+        <div className="absolute top-[-10%] left-0 max-w-[30%] h-[120%] z-31 invisible desktop:visible">
+          <ApplyLeftLog className="w-full h-full" />
+        </div>
+
+        <div className="absolute top-[-10%] right-0 max-w-[30%] h-[120%] z-31 invisible desktop:visible">
+          <ApplyRightLog className="w-full h-full" />
         </div>
       </div>
     </>
