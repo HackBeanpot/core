693546a7cd25996e0e2be677a91e7e090474732f
Aditya <appathak46@gmail.com>
2025-06-14 09:36:14 -0400

    changes to mobile projects view

--- apps/main/src/app/projects/components/Project.tsx
+++ apps/main/src/app/projects/components/Project.tsx
@@ -18,14 +18,18 @@ function Project({
 }: ProjectProps) {
   return (
     <div
-      className="bg-no-repeat justify-center bg-contain bg-center drop-shadow-[0_8px_0px_rgba(0,0,0,.1)] pb-[3rem] hover:scale-90 duration-1000 mb-[.5rem]"
+      className={`
+    bg-no-repeat justify-center bg-contain bg-center 
+    drop-shadow-[0_8px_0px_rgba(0,0,0,.1)] pb-[3rem] 
+    hover:scale-90 duration-1000 mb-[.5rem]
+  `}
       style={{
         backgroundImage: "url('/stamp.svg')",
       }}
     >
       <div className="flex flex-row justify-center max-h-[500px] max-w-[400px] pt-[3rem] pb-[3rem] pr-[5rem] overflow-hidden">
         <div className="">
-          <h2 className="text-[1.75rem] leading-7 text-right font-bold -rotate-90 origin-right font-Wilden-Regular text-[#00877F]">
+          <h2 className=" [@media(max-width:640px)]:rotate-90text-[1.75rem] leading-7 text-right font-bold -rotate-90 origin-right font-Wilden-Regular text-[#00877F]">
             {projectName}
           </h2>
         </div>
@@ -43,7 +47,7 @@ function Project({
           <p className="text-[#918E89] mb-2 mt-5 text-l font-GT-Walsheim-Regular">
             {members}
           </p>
-          <p className="inline-block w-full text-[#333333] text-l font-GT-Walsheim-Regular hyphens-auto text-ellipsis">
+          <p className="inline-block w-full text-[#333333] text-s font-GT-Walsheim-Regular hyphens-auto text-ellipsis">
             {description}
           </p>
         </div>
