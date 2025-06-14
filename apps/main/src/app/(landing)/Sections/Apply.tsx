30cd9495dde77768ad0ffbce40351e91d08107c6
Khushi Khan <76977629+khushikhan0@users.noreply.github.com>
2025-06-13 22:08:25 -0400

    svg bug progress so far

--- apps/main/src/app/(landing)/Sections/Apply.tsx
+++ apps/main/src/app/(landing)/Sections/Apply.tsx
@@ -1,21 +1,21 @@
 "use client";
 
 import React from "react";
-import ApplyLeftLog from "../../lib/Assets/SVG/ApplyAssets/ApplyLeftLog";
-import ApplyRightLog from "../../lib/Assets/SVG/ApplyAssets/ApplyRightLog";
+// import ApplyLeftLog from "../../lib/Assets/SVG/ApplyAssets/ApplyLeftLog";
+// import ApplyRightLog from "../../lib/Assets/SVG/ApplyAssets/ApplyRightLog";
 import ApplyBackground from "../../lib/Assets/SVG/ApplyAssets/ApplyBackground";
 import ApplyLeftBush from "../../lib/Assets/SVG/ApplyAssets/ApplyLeftBush";
-import ApplyRightBush from "../../lib/Assets/SVG/ApplyAssets/ApplyRightBush";
+// import ApplyRightBush from "../../lib/Assets/SVG/ApplyAssets/ApplyRightBush";
 
 const Apply = () => {
   return (
     <>
-      <div className="relative w-full h-[68vh] mt-44 sm:mt-36 mb-14 flex flex-col flex-none">
-        <div className="w-full h-full z-0">
-          <ApplyBackground preserveAspectRatio="xMidYMid slice" />
+      <div className="relative w-full mt-44 sm:mt-36 lg:mt-52 2xl:mt-60 mb-10 flex flex-col flex-none">
+        <div className="w-[110%] left-2 h-full z-0">
+          <ApplyBackground preserveAspectRatio="xMidYMid meet" />
         </div>
 
-        <div className="absolute inset-0 top-[-20%] z-10 flex justify-center items-center px-4 text-center">
+        <div className="absolute left-[50%] top-[-20%] z-10 flex justify-center items-center px-4 text-center">
           <div className="max-w-xl">
             <p className="text-[20px] tablet:desktop:text-2xl desktop:text-3xl font-GT-Walsheim-Regular">
               Applications have not been released for HackBeanpot 2026. Check in
@@ -36,21 +36,21 @@ const Apply = () => {
           </div>
         </div>
 
-        <div className="absolute top-[-5%] left-0 max-w-[120%] h-[130%] z-31 invisible desktop:visible">
-          <ApplyLeftBush className="w-full h-full" />
+        <div className="absolute top-[-5%] left-0 max-w-[120%] z-31 invisible desktop:visible">
+          <ApplyLeftBush className="w-[20%] h-[20%]" />
         </div>
 
-        <div className="absolute top-[-15%] right-[-2%] max-w-[120%] h-[120%] z-31 invisible desktop:visible">
+        {/* <div className="absolute top-[-15%] right-[-2%] max-w-[120%] h-[120%] z-31 invisible desktop:visible">
           <ApplyRightBush className="w-full h-full" />
-        </div>
+        </div> */}
 
-        <div className="absolute top-[-10%] left-0 max-w-[30%] h-[120%] z-31 invisible desktop:visible">
+        {/* <div className="absolute top-[-10%] left-0 max-w-[30%] h-[120%] z-31 invisible desktop:visible">
           <ApplyLeftLog className="w-full h-full" />
-        </div>
+        </div> */}
 
-        <div className="absolute top-[-10%] right-0 max-w-[30%] h-[120%] z-31 invisible desktop:visible">
+        {/* <div className="absolute top-[-10%] right-0 max-w-[30%] h-[120%] z-31 invisible desktop:visible">
           <ApplyRightLog className="w-full h-full" />
-        </div>
+        </div> */}
       </div>
     </>
   );
