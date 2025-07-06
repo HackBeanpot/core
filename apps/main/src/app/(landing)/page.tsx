7f7b6aa0b976e4594bc0b2542815ee424cc3dd6a
Alina <55455721+agonzalez22@users.noreply.github.com>
2025-07-06 13:52:46 -0400

    changed landing styling

--- apps/main/src/app/(landing)/page.tsx
+++ apps/main/src/app/(landing)/page.tsx
@@ -1,37 +1,37 @@
-// import {
-//   Stats,
-//   Values,
-//   FAQSection,
-//   Landing,
-//   Keynote,
-//   Calendar,
-//   Testimonials,
-//   Apply,
-//   About,
-// } from "./Sections";
-// import { Footer, NavBar } from "../lib/Components";
-// import Head from "next/head";
+import {
+  Stats,
+  Values,
+  FAQSection,
+  Landing,
+  Keynote,
+  Calendar,
+  Testimonials,
+  Apply,
+  About,
+} from "./Sections";
+import { Footer, NavBar } from "../lib/Components";
+import Head from "next/head";
 import React from "react";
-import Placeholder from "@repo/ui/Placeholder";
+// import Placeholder from "@repo/ui/Placeholder";
 
 export default function Page(): JSX.Element {
   return (
-    // <main className="flex flex-col items-center min-h-screen bg-light-yellow">
-    //   <Head>
-    //     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    //   </Head>
-    //   <NavBar />
-    //   <Landing />
-    //   <Apply />
-    //   <Stats />
-    //   <About />
-    //   <Values />
-    //   <Keynote />
-    //   <Calendar />
-    //   <Testimonials />
-    //   <FAQSection />
-    //   <Footer />
-    // </main>
-    <Placeholder />
+    <main className="flex flex-col items-center min-h-screen bg-light-yellow">
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <NavBar />
+      <Landing />
+      <Apply />
+      <Stats />
+      <About />
+      <Values />
+      <Keynote />
+      <Calendar />
+      <Testimonials />
+      <FAQSection />
+      <Footer />
+    </main>
+    // <Placeholder />
   );
 }
