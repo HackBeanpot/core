420e418c5fb7f81ab3940a987d204b8575b4994c
Alexandra Hu <108170281+allexandrahu@users.noreply.github.com>
2025-07-08 22:58:07 -0400

    palceholder

--- apps/live/src/app/page.tsx
+++ apps/live/src/app/page.tsx
@@ -5,7 +5,7 @@ import MentorSection from "./Sections/Mentors";
 import OurTeamBackground from "./Sections/OurTeamBackground";
 import HitTheRoad from "./Sections/HitTheRoad";
 import Resources from "./Sections/Resources";
-import NavBar from "./lib/Components/NavBar";
+// import NavBar from "./lib/Components/NavBar";
 import Footer from "./lib/Components/Footer";
 import ComingUp from "./Sections/ComingUp/ComingUp";
 import OurTeam from "./Sections/OurTeam";
@@ -14,7 +14,7 @@ import Keynote from "./Sections/Keynote";
 export default function Page(): JSX.Element {
   return (
     <main className="flex flex-col items-center">
-      <NavBar />
+      {/* <NavBar />*/}
       <Landing />
       <ComingUp />
       <HitTheRoad />
