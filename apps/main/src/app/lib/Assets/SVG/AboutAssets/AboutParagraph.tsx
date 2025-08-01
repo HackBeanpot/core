5b73d6e88b146064012f0443b406c6477b8f372e
adityapat24 <appathak46@gmail.com>
2025-08-01 14:08:05 -0400

    changes to about paragraph

--- apps/main/src/app/lib/Assets/SVG/AboutAssets/AboutParagraph.tsx
+++ apps/main/src/app/lib/Assets/SVG/AboutAssets/AboutParagraph.tsx
@@ -10,9 +10,16 @@ const AboutParagraph = () => {
     mobile:mt-4 tablet:mt-12 desktop:mt-12
     text-left 
     w-[70vw]
-    text-[11px]               
+    mobile:text-[11px]
+    mobilelg:text-[13px]
+    tablet:text-[11px]
+    desktop:text-[11px]
     tablet:text-3xl           
     tablet:max-w-full
+    mobile:-ml-10
+    mobilelg:-ml-3
+    tablet:ml-0
+    desktop:ml-0
   "
       >
         We&apos;re a non-profit organization in the Boston area that organizes
