ed37e0301baba94536b4c447cda08f99ab6a248a
Khushi Khan <76977629+khushikhan0@users.noreply.github.com>
2025-08-02 04:12:26 -0400

    Added the original background color back into the recruitment section

--- apps/main/src/app/sponsor-us/recruitment.tsx
+++ apps/main/src/app/sponsor-us/recruitment.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from "react";
 
 const RecruitmentBenefits = () => {
   return (
-    <div className="bg-[#000000] w-full flex items-center justify-center">
+    <div className="bg-[#F9EFDA] w-full flex items-center justify-center">
       <div className="bg-[#0D685F] rounded-2xl p-6 tablet:p-8 text-white w-[866px] h-[584.44px]">
         <h2 className="text-center text-3xl font-GT-Walsheim-Trial font-medium mb-8">
           How can sponsoring HackBeanpot help your company?
@@ -13,7 +13,9 @@ const RecruitmentBenefits = () => {
               Recruitment
             </h3>
             <p className="font-GT-Walsheim-Trial text-md mb-6">
-              Scout the next generation of talent here. Sponsoring HackBeanpot is a great way to expand and diversify your company&apos;s full-time, internship, co-op program&apos;s applicant pool!
+              Scout the next generation of talent here. Sponsoring HackBeanpot
+              is a great way to expand and diversify your company&apos;s
+              full-time, internship, co-op program&apos;s applicant pool!
             </p>
             <div className="relative top-6">
               <img src="/image.png" />
@@ -24,7 +26,10 @@ const RecruitmentBenefits = () => {
               Market your company & product
             </h3>
             <p className="font-GT-Walsheim-Trial text-md mb-6">
-              Market your product and receive feedback by sponsoring a company-specific prize, hosting a workshop, giving a product demo, or mentoring hackers that are eager to hear your advice and implement your technologies for their projects.
+              Market your product and receive feedback by sponsoring a
+              company-specific prize, hosting a workshop, giving a product demo,
+              or mentoring hackers that are eager to hear your advice and
+              implement your technologies for their projects.
             </p>
             <div className="relative">
               <img src="/image2.png" />
