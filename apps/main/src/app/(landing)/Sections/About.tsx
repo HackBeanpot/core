2cc0f1f5d3160f8026cff593180c6b4129b030dc
adityapat24 <appathak46@gmail.com>
2025-08-02 19:54:36 -0400

    removed absolute

--- apps/main/src/app/(landing)/Sections/About.tsx
+++ apps/main/src/app/(landing)/Sections/About.tsx
@@ -51,11 +51,11 @@ export default function About(): React.ReactNode {
           </button>
         </div>
         <div
-          className="absolute
+          className="
                  w-[560.34px] h-[132.06px] left-[-160.67px] 
                  tablet:absolute tablet:w-[312px] tablet:h-auto tablet:left-[66%] 
-                 tablet:bottom-auto tablet:top-[62vh] tablet:scale-[3.3] 
-                 mobile:mt-5 mobile:ml-5 mobile:bottom-0
+                 tablet:bottom-auto tablet:top-[64vh] tablet:scale-[3.3] 
+                 mobile:mt-1 mobile:ml-5 mobile:bottom-0
                  mobilelg:bottom-0"
         >
           <Cities />
