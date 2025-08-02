9b3f57334c68c9ed14db42da6acf1b8c78d65234
Khushi Khan <76977629+khushikhan0@users.noreply.github.com>
2025-08-02 14:48:34 -0400

    Got rid of example sponsor props, it's in history now so we can refer to it later again if we need it

--- apps/main/src/app/sponsor-us/recruitment.tsx
+++ apps/main/src/app/sponsor-us/recruitment.tsx
@@ -1,21 +1,5 @@
 import React from "react";
 
-// const sponsorProps: React.ReactNode = <div className="">
-//   <div className="relative grid grid-cols-3 grid-flow-dense ml-5 mt-5">
-//     <div className="scale-150 ml-8 mt-1"><img src="/sponsor-images/google_logo.svg" alt="Google logo" /></div>
-//     <div className="scale-150 ml-7"><img src="/sponsor-images/datadog_logo.svg" alt="Datadog logo" /></div>
-//     <div className="scale-150 mt-2"><img src="/sponsor-images/simplisafe_logo.svg" alt="Simplisafe logo" /></div>
-//     <div className="col-span-2 scale-150 ml-10"><img src="/sponsor-images/meta_logo.svg" alt="Meta logo" /></div>
-//     <div className="scale-150"><img src="/sponsor-images/woodmack_logo.svg" alt="Wood Mackenzie logo" /></div>
-//     <div className="scale-125 mt-2 ml-4"><img src="/sponsor-images/upstate_logo.svg" alt="Upstatement logo" /></div>
-//     <div className="scale-125 ml-3"><img src="/sponsor-images/yelp_logo.svg" alt="Yelp logo" /></div>
-//     <div className="mt-8 scale-150"><img src="/sponsor-images/vmware_logo.svg" alt="Vmware logo" /></div>
-//     <div><img src="/sponsor-images/cargurus_logo.svg" alt="Cargurus logo" /></div>
-//     <div className="ml-10 scale-150"><img src="/sponsor-images/toast_logo.svg" alt="Toast logo" /></div>
-//   </div>
-//   <p className="mt-10 font-medium text-lg" style={{ "fontStyle": "italic"}}>Due to HackBeanpot&apos;s status as a 501c3 nonprofit organization, all sponsorship package purchases are considered tax deductible</p>
-// </div>
-
 const RecruitmentBenefits = () => {
   return (
     <div className="bg-[#F9EFDA] w-full flex items-center justify-center">
