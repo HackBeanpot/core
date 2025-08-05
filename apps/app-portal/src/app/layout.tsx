d8fbc667d0bcb63509134be8fc3c65005c6a95ed
Alina <55455721+agonzalez22@users.noreply.github.com>
2025-08-04 22:43:57 -0400

    initial setup of app portal

---
+++ apps/app-portal/src/app/layout.tsx
@@ -0,0 +1,29 @@
+import "./globals.css";
+import "../../../../packages/util/src/fonts/fonts.css";
+import "@repo/ui/styles.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import React from "react";
+import { Providers } from "./providers";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "HackBeanpot - Application Portal",
+  description:
+    "Apply to HackBeanpot!",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}): JSX.Element {
+  return (
+    <html lang="en" className="w-screen overflow-x-hidden">
+      <body className={inter.className}>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}
