fb3bf6e5ebeb3a4d2753de9dc46c1cadecd0dc9a
jjams <jjams0915@gmail.com>
2025-09-10 21:40:50 -0400

    Added StatsPage svg and implemented Stats.tsx

--- apps/main/src/app/(landing)/Sections/Stats.tsx
+++ apps/main/src/app/(landing)/Sections/Stats.tsx
@@ -1,9 +1,14 @@
 "use client";
 
 import React from "react";
+import StatsPage from "../../lib/Assets/SVG/StatsPage";
 // import clsx from "clsx";
 // import useDevice from "@repo/util/hooks/useDevice";
 
 export default function Stats(): React.ReactNode {
-  return <></>;
+  return (
+      <div className="w-screen h-screen overflow-hidden">
+        <StatsPage className="w-full h-auto"/>
+      </div>
+  );
 }
