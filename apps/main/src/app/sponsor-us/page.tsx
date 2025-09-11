f8b483833b92583b04b23b7b850b379ed75d4843
Yumiko (Yumi) Chow <75456756+yumi520@users.noreply.github.com>
2025-09-10 22:31:03 -0400

    merged from main

--- apps/main/src/app/sponsor-us/page.tsx
+++ apps/main/src/app/sponsor-us/page.tsx
@@ -3,7 +3,9 @@ import React from "react";
 // import { Footer } from "../lib/Components";
 const SponsorUsPage = () => {
   return (
-    <div className="relative z-10 w-full flex flex-col items-center"></div>
+    <div className="relative z-10 w-full flex flex-col items-center">
+      <h1>sponsor us!</h1>
+    </div>
   );
 };
 export default SponsorUsPage;
