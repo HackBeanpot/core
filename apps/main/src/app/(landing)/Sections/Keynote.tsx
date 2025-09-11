42797bff251e91659d4828a3f42d1e6adc630be7
adityapat24 <appathak46@gmail.com>
2025-09-11 16:56:48 -0400

    format

--- apps/main/src/app/(landing)/Sections/Keynote.tsx
+++ apps/main/src/app/(landing)/Sections/Keynote.tsx
@@ -12,21 +12,21 @@ export default function Keynote(): React.ReactNode {
   return (
     <>
       <div className="border relative flex flex-col items-center justify-center w-full">
-        <KeynoteSpeaker className="border"/>
+        <KeynoteSpeaker className="border" />
         <div className="border desktop:mx-[20%] tablet:top-[105%] absolute top-[55%] flex lg:flex-row sm:flex-col justify-center desktop:gap-8">
-            <GuestPhoto className="border w-80 h-80" />
-            <div className="border max-w-lg text-left tablet:space-y-1 desktop:space-y-2">
-              <div className="tablet:text-charcoalFog desktop:text-white text-3xl font-semibold font-['NeulisNeue-Bold'] tablet:leading-1 desktop:leading-relaxed">
-                Jamie Chen
-              </div>
-              <div className="tablet:text-charcoalFog tablet:text-[14px] desktop:text-white desktop:text-2xl text-base font-light font-['DMSans-Regular'] tablet:leading-1 desktop:leading-relaxed">
-                Jamie Chen is Director of Product Engineering at Luma Labs, where
-                she leads teams building ethical, user-focused AI tools. With a
-                background in computer science and over a decade in tech, she&apos;s
-                known for her leadership in inclusive innovation. Jamie also mentors
-                emerging engineers and speaks on ethical development and tech for
-                social good.
-              </div>
+          <GuestPhoto className="border w-80 h-80" />
+          <div className="border max-w-lg text-left tablet:space-y-1 desktop:space-y-2">
+            <div className="tablet:text-charcoalFog desktop:text-white text-3xl font-semibold font-['NeulisNeue-Bold'] tablet:leading-1 desktop:leading-relaxed">
+              Jamie Chen
+            </div>
+            <div className="tablet:text-charcoalFog tablet:text-[14px] desktop:text-white desktop:text-2xl text-base font-light font-['DMSans-Regular'] tablet:leading-1 desktop:leading-relaxed">
+              Jamie Chen is Director of Product Engineering at Luma Labs, where
+              she leads teams building ethical, user-focused AI tools. With a
+              background in computer science and over a decade in tech,
+              she&apos;s known for her leadership in inclusive innovation. Jamie
+              also mentors emerging engineers and speaks on ethical development
+              and tech for social good.
+            </div>
           </div>
         </div>
       </div>
