2084bf150a745e2cc6199c2d1904bda9cb64f062
yurikakan <kan.yu@northeastern.edu>
2025-09-11 13:22:42 -0400

    Responsive Formatting cont...

--- apps/main/src/app/(landing)/Sections/Keynote.tsx
+++ apps/main/src/app/(landing)/Sections/Keynote.tsx
@@ -12,21 +12,21 @@ export default function Keynote(): React.ReactNode {
   return (
     <>
       <div className="border relative flex flex-col items-center justify-center w-full">
-        <KeynoteSpeaker className="border"/>
+        <KeynoteSpeaker className="border" />
         <div className="border mx-[20%] absolute top-[55%] flex lg:flex-row sm:flex-col justify-center gap-8">
-            <GuestPhoto className="border w-80 h-80" />
-            <div className="border max-w-lg text-left space-y-2">
-              <div className="text-white text-3xl font-semibold font-['NeulisNeue-Bold'] leading-relaxed">
-                Jamie Chen
-              </div>
-              <div className="text-white text-base font-light font-['DMSans-Regular'] leading-relaxed">
-                Jamie Chen is Director of Product Engineering at Luma Labs, where
-                she leads teams building ethical, user-focused AI tools. With a
-                background in computer science and over a decade in tech, she&apos;s
-                known for her leadership in inclusive innovation. Jamie also mentors
-                emerging engineers and speaks on ethical development and tech for
-                social good.
-              </div>
+          <GuestPhoto className="border w-80 h-80" />
+          <div className="border max-w-lg text-left space-y-2">
+            <div className="text-white text-3xl font-semibold font-['NeulisNeue-Bold'] leading-relaxed">
+              Jamie Chen
+            </div>
+            <div className="text-white text-base font-light font-['DMSans-Regular'] leading-relaxed">
+              Jamie Chen is Director of Product Engineering at Luma Labs, where
+              she leads teams building ethical, user-focused AI tools. With a
+              background in computer science and over a decade in tech,
+              she&apos;s known for her leadership in inclusive innovation. Jamie
+              also mentors emerging engineers and speaks on ethical development
+              and tech for social good.
+            </div>
           </div>
         </div>
       </div>
