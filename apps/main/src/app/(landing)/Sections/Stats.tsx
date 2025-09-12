c3bd45bb449a3c0aa050660889c5cead6220cab2
swarkewalia <145241312+swarkewalia@users.noreply.github.com>
2025-09-11 22:27:28 -0400

    added breakpoints

--- apps/main/src/app/(landing)/Sections/Stats.tsx
+++ apps/main/src/app/(landing)/Sections/Stats.tsx
@@ -7,7 +7,13 @@ import StatsPage from "../../lib/Assets/SVG/StatsPage";
 
 export default function Stats(): React.ReactNode {
   return (
-    <div className="w-screen h-screen overflow-hidden">
+    <div className="
+          w-[90%] h-[90%]        
+          tablet:w-[100%] h-auto       
+          desktop:w-[100%]       
+          desktop-xl:w-[100%]    
+          desktop-2xl:w-[100%]   
+        ">
       <StatsPage className="w-full h-auto" />
     </div>
   );
