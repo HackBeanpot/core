faf6b4596d7e7e3061f185c44611341f229a2e97
Yumiko (Yumi) Chow <75456756+yumi520@users.noreply.github.com>
2025-09-12 00:34:32 -0400

    tried to fix responsiveness

--- apps/main/src/app/sponsor-us/page.tsx
+++ apps/main/src/app/sponsor-us/page.tsx
@@ -14,10 +14,10 @@ const SponsorUsPage = () => {
         <div className="pointer-events-none absolute inset-0 flex">
           <div className="ml-[40%] w-[min(40rem,45%)] self-center">
             <div className="pointer-events-auto flex flex-col items-start text-left">
-              <h1 className="text-4xl mb-3 text-charcoalFog font-NeulisNeue-Bold">
+              <h1 className="text-4xl md:text-3xl sm:text-2xl mb-3 text-charcoalFog font-NeulisNeue-Bold">
                 Interested in sponsoring us?
               </h1>
-              <p className="text-2xl text-charcoalFog mb-6 font-NeulisNeue-Regular leading-relaxed">
+              <p className="text-2xl md:text-lg sm:text-base text-charcoalFog mb-6 font-NeulisNeue-Regular leading-relaxed">
                 Reach out to us at team@hackbeanpot.com
                 <br /> or check out our sponsorship package!
                 {/* TODO: add link to sponsorship packet */}
