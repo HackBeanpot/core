cfa1f8b8fa0d92bfd571aff29e878cfb5c86df26
liviacutra <liviacutra@northeastern.edu>
2025-09-11 23:14:30 -0400

    perfected layout for medium responsive + spacing

--- apps/main/src/app/sponsor-us/page.tsx
+++ apps/main/src/app/sponsor-us/page.tsx
@@ -5,31 +5,34 @@ import TicketBooth from "../lib/Assets/SVG/TicketBooth";
 import Button from "@repo/ui/Button";
 
 const SponsorUsPage = () => {
-  return (
-    <div className="relative z-10 w-full flex flex-col items-center">
-      <div className="relative w-full flex flex-col items-center">
-        <div className="w-full flex justify-center items-center">
+   return (
+    <div className="relative z-10 w-full">
+      <div className="relative mx-auto w-full max-w-[1200px]">
+        <div className="w-full flex justify-center">
           <TicketBooth />
         </div>
-        <div className="absolute top-96 left-36 w-full flex flex-col items-center ">
-          <h1 className="text-4xl mt-8 mb-2 bg-opacity-80 px-4 py-2 rounded text-charcoalFog font-NeulisNeue-Bold">
-            Interested in sponsoring us?
-          </h1>
-          <p className="text-xl text-charcoalFog mb-2 bg-opacity-80 px-4 py-2 font-NeulisNeue-Regular">
-            Reach out to us at team@hackbeanpot.com
-            <br /> or check out our sponsorship package!
-          </p>
-          {/* TODO: add link to sponsorship packet */}
-          <div className="pointer-events-auto">
-            <Button
-              text="View Sponsorship Package"
-              color="starlightBlue"
-              textColor="white"
-            />
+        <div className="pointer-events-none absolute inset-0 flex">
+          <div className="ml-[40%] w-[min(40rem,45%)] self-center">
+            <div className="pointer-events-auto flex flex-col items-start text-left">
+              <h1 className="text-4xl mb-3 text-charcoalFog font-NeulisNeue-Bold">
+                Interested in sponsoring us?
+              </h1>
+              <p className="text-2xl text-charcoalFog mb-6 font-NeulisNeue-Regular leading-relaxed">
+                Reach out to us at team@hackbeanpot.com
+                <br /> or check out our sponsorship package!
+                {/* TODO: add link to sponsorship packet */}
+              </p>
+              <Button
+                text="View Sponsorship Package"
+                color="starlightBlue"
+                textColor="white"
+              />
+            </div>
           </div>
         </div>
       </div>
     </div>
   );
 };
+
 export default SponsorUsPage;
