0577549c1f4770a851e2d63121d8d2c036554b16
Yumiko (Yumi) Chow <75456756+yumi520@users.noreply.github.com>
2025-09-13 20:27:12 -0400

    changed jsx to tsx and fixed responsiveness

--- apps/main/src/app/sponsor-us/page.tsx
+++ apps/main/src/app/sponsor-us/page.tsx
@@ -5,7 +5,7 @@ import TicketBooth from "../lib/Assets/SVG/TicketBooth";
 import Button from "@repo/ui/Button";
 
 const SponsorUsPage = () => {
-   return (
+  return (
     <div className="relative z-10 w-full">
       <div className="relative mx-auto w-full max-w-[1200px]">
         <div className="w-full flex justify-center">
@@ -15,12 +15,12 @@ const SponsorUsPage = () => {
           <div className="ml-4 w-[90%] self-center">
             <div className="flex flex-col items-start ml-[44%]">
               <h1 className="text-[clamp(1.25rem,2vw+1rem,2.5rem)] mb-3 text-charcoalFog font-NeulisNeue-Bold">
-                  Interested in sponsoring us?
+                Interested in sponsoring us?
               </h1>
-                <p className="text-[clamp(1rem,1.5vw+0.5rem,1.5rem)] text-charcoalFog mb-6 font-NeulisNeue-Regular leading-relaxed">
-                  Reach out to us at team@hackbeanpot.com
-                  <br /> or check out our sponsorship package!
-                </p>
+              <p className="text-[clamp(1rem,1.5vw+0.5rem,1.5rem)] text-charcoalFog mb-6 font-NeulisNeue-Regular leading-relaxed">
+                Reach out to us at team@hackbeanpot.com
+                <br /> or check out our sponsorship package!
+              </p>
               <Button
                 text="View Sponsorship Package"
                 color="starlightBlue"
