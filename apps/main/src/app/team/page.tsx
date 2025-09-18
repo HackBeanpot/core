8b1ceb1dd373343acef27cfc872770bc77f78e20
Alexandra Hu <108170281+allexandrahu@users.noreply.github.com>
2025-09-18 02:02:34 -0400

    Our Team Topmost Blurb #234

--- apps/main/src/app/team/page.tsx
+++ apps/main/src/app/team/page.tsx
@@ -2,11 +2,13 @@ import { Footer, NavBar } from "../lib/Components";
 import React from "react";
 import Teams from "./Sections/Teams";
 import Landing from "./Sections/Landing";
+import Hero from "../lib/Assets/SVG/hero";
 
 export default function Page(): JSX.Element {
   return (
     <main className="flex flex-col items-center min-h-screen">
       <NavBar />
+      <Hero />
       <Landing />
       <Teams />
       <Footer />
