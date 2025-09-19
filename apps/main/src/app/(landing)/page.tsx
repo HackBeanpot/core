ca96a03e335eb6491954398ccaad8d2998c858d7
alex_chen_04 <127902759+Kloafe@users.noreply.github.com>
2025-09-18 21:54:29 -0400

    icon

--- apps/main/src/app/(landing)/page.tsx
+++ apps/main/src/app/(landing)/page.tsx
@@ -9,20 +9,26 @@ import {
   Apply,
   About,
 } from "./Sections";
-import { Footer, NavBar } from "../lib/Components";
+import { Footer, NavBar} from "../lib/Components";
 import Head from "next/head";
 import React from "react";
+import Icon from "../lib/Assets/SVG/Icon";
+
+
 
 export default function Page(): JSX.Element {
   return (
+
     <main className="flex flex-col items-center min-h-screen bg-canopyGreen">
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
+
       <NavBar />
       <Landing />
       <Apply />
       <Stats />
+      <Icon image_location="/headshots/emma_icon.png" url = 'https://www.linkedin.com/in/alexchen04/' />
       <About />
       <Values />
       <Keynote />
