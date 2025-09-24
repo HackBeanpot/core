b4091d3a7e3f5e1f3065a8b3c90ff1b494fb90ee
adityapat24 <appathak46@gmail.com>
2025-09-24 14:08:31 -0400

    initial svgs

--- apps/main/src/app/(landing)/Sections/About.tsx
+++ apps/main/src/app/(landing)/Sections/About.tsx
@@ -3,11 +3,15 @@
 import React from "react";
 // import Button from "@repo/ui/Button";
 // import Footer from "../../lib/Components/Footer";
-
+import TextBackground from "../../lib/Assets/AboutLandingAssets/text";
 // import clsx from "clsx";
 // import useDevice from "@repo/util/hooks/useDevice";
 
 export default function About(): React.ReactNode {
   // example for buttons, remove whenever
-  return <></>;
+  return (
+    <>
+      <TextBackground />
+    </>
+  );
 }
---
+++ apps/main/src/app/lib/Assets/AboutLandingAssets/teamPicture.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+const TeamPicture: React.FC = () => {
+  return (
+    <svg
+      width="538"
+      height="352"
+      viewBox="0 0 538 352"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g filter="url(#filter0_d_707_13984)">
+        <path d="M527 17.1854L0.96875 0V341.538H527V17.1854Z" fill="white" />
+      </g>
+      <defs>
+        <filter
+          id="filter0_d_707_13984"
+          x="0.96875"
+          y="0"
+          width="536.288"
+          height="351.794"
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dx="10.2564" dy="10.2564" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.2 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_707_13984"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_707_13984"
+            result="shape"
+          />
+        </filter>
+      </defs>
+    </svg>
+  );
+};
+export default TeamPicture;
