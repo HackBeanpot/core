ed446a8578ba5ff7f1e0fc801955bd817cedccee
alex_chen_04 <127902759+Kloafe@users.noreply.github.com>
2025-09-24 17:23:54 -0400

    testing

---
+++ apps/main/src/app/(landing)/Sponsors.tsx
@@ -0,0 +1,16 @@
+import {
+  Stats,
+  Values,
+  FAQSection,
+  Landing,
+  Keynote,
+  Calendar,
+  Testimonials,
+  Apply,
+  About,
+} from "./Sections";
+import { Footer, NavBar } from "../lib/Components";
+import Head from "next/head";
+import React from "react";
+
+
