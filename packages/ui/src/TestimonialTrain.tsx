ed67091ee34b88d4fac36b8dc4451fcd5015ba3d
khushi khan <76977629+khushikhan0@users.noreply.github.com>
2025-10-06 23:02:42 -0400

    Update TestimonialTrain.tsx

--- packages/ui/src/TestimonialTrain.tsx
+++ packages/ui/src/TestimonialTrain.tsx
@@ -1,54 +1,69 @@
 "use client";
 
-import React, { useEffect, useRef, useState, useCallback} from "react";
+import React, { useState, useCallback } from "react";
 import Train from "./Train";
 import Button from "./Button";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import useDevice from "@repo/util/hooks/useDevice";
 // import clsx from "clsx";
 
+const deviceBounds = [
+  { leftBound: 0, rightBound: 1400, threshold: 500 }, // isMobile
+  { leftBound: 0, rightBound: 1400, threshold: 600 }, // isTablet
+  { leftBound: 0, rightBound: 1400, threshold: 650 }, // isDesktop
+];
+
 export default function TestimonialTrain() {
-const [pixelPos, setPixelPos] = useState(-1400);
+  const { isMobile, isTablet, isDesktop } = useDevice();
+  const [pixelPos, setPixelPos] = useState(1400);
+  const [boundsID, setBoundsID] = useState(2);
+
+  const currBounds = deviceBounds[boundsID];
+
+  const handleBounds = useCallback(() => {
+    setBoundsID(isMobile ? 0 : isTablet ? 1 : 2);
+  }, [isMobile, isTablet, isDesktop]);
 
   const handleLeftClick = useCallback(() => {
-    setPixelPos(prev => prev + 700);
-    // console.log(pixelPos);
-  }, []);
+    if (pixelPos > currBounds.leftBound) {
+      setPixelPos((prev) => prev - currBounds.threshold);
+    }
+    // console.log(pixelPos + " - threshold: " + currBounds.threshold);
+  }, [pixelPos]);
 
   const handleRightClick = useCallback(() => {
-    setPixelPos(prev => prev - 700);
+    if (pixelPos < currBounds.rightBound) {
+      setPixelPos((prev) => prev + currBounds.threshold);
+    }
     // console.log(pixelPos);
-  }, []);
-
-  useEffect(() => {
-    console.log(pixelPos);
   }, [pixelPos]);
 
   return (
     <div className="flex max-w-screen h-[400px]">
       <div className="relative overflow-hidden w-screen z-0 bg-tomato">
-          <Train 
-              className={`h-full transition-transform translate-x-[${pixelPos}px] ease-in-out duration-300`}
+        <Train
+          className={`h-full transition-transform ease-in-out duration-300`}
+          style={{ transform: `translateX(-${pixelPos}px)` }}
         />
-        
       </div>
       {/* top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 */}
       <div className="absolute w-full z-10 self-center">
-          <div className="flex flex-row items-center justify-center gap-[550px]">
-              <Button 
-                  color="ribbonBlue"
-                  textColor="starlightBlue"
-                  icon={<IoIosArrowBack size={28} />} 
-                  removePadding={true}   
-                  onClick={() => handleLeftClick()}
-              />
-              <Button 
-                  color="ribbonBlue"
-                  textColor="starlightBlue"
-                  icon={<IoIosArrowForward size={28} />}    
-                  removePadding={true}
-                  onClick={() => handleRightClick()}
-              />
-          </div>
+        <div className="flex flex-row items-center justify-center gap-[550px]">
+          <Button
+            color="ribbonBlue"
+            textColor="starlightBlue"
+            icon={<IoIosArrowBack size={28} />}
+            removePadding={true}
+            onClick={handleLeftClick}
+          />
+          <Button
+            color="ribbonBlue"
+            textColor="starlightBlue"
+            icon={<IoIosArrowForward size={28} />}
+            removePadding={true}
+            onClick={handleRightClick}
+          />
+        </div>
       </div>
     </div>
   );
