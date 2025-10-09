7d220ec5411ba0729ad6e619cb6364b831b8d4d3
Khushi Khan <76977629+khushikhan0@users.noreply.github.com>
2025-10-08 23:13:24 -0400

    current train progress

--- apps/main/src/app/(landing)/page.tsx
+++ apps/main/src/app/(landing)/page.tsx
@@ -1,15 +1,15 @@
 import {
-  Stats,
-  Values,
-  FAQSection,
-  Landing,
-  Keynote,
-  Calendar,
-  Testimonials,
-  Apply,
+  // Stats,
+  // Values,
+  // FAQSection,
+  // Landing,
+  // Keynote,
+  // Calendar,
+  // Testimonials,
+  // Apply,
   About,
 } from "./Sections";
-import { Footer, NavBar } from "../lib/Components";
+import { Footer } from "../lib/Components";
 import Head from "next/head";
 import React from "react";
 
--- packages/ui/src/TestimonialTrain.tsx
+++ packages/ui/src/TestimonialTrain.tsx
@@ -1,54 +1,72 @@
 "use client";
 
-import React, { useEffect, useRef, useState, useCallback} from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import Train from "./Train";
+// import TrainTracks from "./TestimonialAssets/TrainTracks";
 import Button from "./Button";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import useDevice from "@repo/util/hooks/useDevice";
 // import clsx from "clsx";
 
+const deviceBounds = [
+  { leftBound: 0, rightBound: 1400, threshold: 500 }, // isMobile
+  { leftBound: 0, rightBound: 1400, threshold: 600 }, // isTablet
+  { leftBound: 0, rightBound: 1400, threshold: 650 }, // isDesktop
+];
+
 export default function TestimonialTrain() {
-const [pixelPos, setPixelPos] = useState(-1400);
+  const { isMobile, isTablet } = useDevice();
+  const [pixelPos, setPixelPos] = useState(1400);
+  const [boundsID, setBoundsID] = useState(2);
 
-  const handleLeftClick = useCallback(() => {
-    setPixelPos(prev => prev + 700);
-    // console.log(pixelPos);
-  }, []);
-
-  const handleRightClick = useCallback(() => {
-    setPixelPos(prev => prev - 700);
-    // console.log(pixelPos);
-  }, []);
+  const currBounds = deviceBounds[boundsID];
 
   useEffect(() => {
-    console.log(pixelPos);
-  }, [pixelPos]);
+    setBoundsID(isMobile ? 0 : isTablet ? 1 : 2);
+  }, [isMobile, isTablet]);
+
+  const handleLeftClick = useCallback(() => {
+    if (pixelPos > currBounds.leftBound + currBounds.threshold) {
+      setPixelPos((prev) => prev - currBounds.threshold);
+    }
+  }, [pixelPos, currBounds]);
+
+  const handleRightClick = useCallback(() => {
+    if (pixelPos < currBounds.rightBound) {
+      setPixelPos((prev) => prev + currBounds.threshold);
+    }
+    // console.log(pixelPos);
+  }, [pixelPos, currBounds]);
 
   return (
     <div className="flex max-w-screen h-[400px]">
-      <div className="relative overflow-hidden w-screen z-0 bg-tomato">
-          <Train 
-              className={`h-full transition-transform translate-x-[${pixelPos}px] ease-in-out duration-300`}
+      <div className="relative overflow-hidden w-screen z-10 bg-tomato p-12">
+        <Train
+          className={`h-full transition-transform ease-in-out duration-300`}
+          style={{ transform: `translateX(-${pixelPos}px)` }}
         />
-        
       </div>
+      {/* <div className="relative w-full">
+        <TrainTracks className="absolute z-10 bottom-10 w-fit" />
+      </div> */}
       {/* top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 */}
       <div className="absolute w-full z-10 self-center">
-          <div className="flex flex-row items-center justify-center gap-[550px]">
-              <Button 
-                  color="ribbonBlue"
-                  textColor="starlightBlue"
-                  icon={<IoIosArrowBack size={28} />} 
-                  removePadding={true}   
-                  onClick={() => handleLeftClick()}
-              />
-              <Button 
-                  color="ribbonBlue"
-                  textColor="starlightBlue"
-                  icon={<IoIosArrowForward size={28} />}    
-                  removePadding={true}
-                  onClick={() => handleRightClick()}
-              />
-          </div>
+        <div className="flex flex-row items-center justify-center gap-[550px]">
+          <Button
+            color="ribbonBlue"
+            textColor="starlightBlue"
+            icon={<IoIosArrowBack size={28} />}
+            removePadding={true}
+            onClick={handleLeftClick}
+          />
+          <Button
+            color="ribbonBlue"
+            textColor="starlightBlue"
+            icon={<IoIosArrowForward size={28} />}
+            removePadding={true}
+            onClick={handleRightClick}
+          />
+        </div>
       </div>
     </div>
   );
