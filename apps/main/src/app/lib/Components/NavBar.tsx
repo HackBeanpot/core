0921122305223d91b7ab37d30cec56a5da400c4d
Khushi Khan <76977629+khushikhan0@users.noreply.github.com>
2025-10-14 00:05:24 -0400

    implemented MD and SM view, will simplify logic later

--- apps/main/src/app/lib/Components/NavBar.tsx
+++ apps/main/src/app/lib/Components/NavBar.tsx
@@ -1,20 +1,20 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../Assets/SVG/HackBeanpotLogo";
 import LocalLink from "./LocalLink";
 import Button from "@repo/ui/Button";
-// import HomeIcon from "@repo/ui/Icons/HomeIcon";
-// import Hamburger from "hamburger-react";
+import useDevice from "@util/hooks/useDevice";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 const NavBar = () => {
-  // const [isOpen, setOpen] = useState(false);
-  // const isMobile = useIsMobileLgScreen();
+  const [isOpen, setOpen] = useState(false);
+  const { isDesktop } = useDevice();
 
   type LocalLinkInfo = {
     pageName: string;
     link: string;
-  }
+  };
 
   const localLinkArr: LocalLinkInfo[] = [
     { pageName: "About", link: "/" },
@@ -24,35 +24,73 @@ const NavBar = () => {
   ];
 
   return (
-    <div className="sticky top-0 z-[50] w-full h-fit bg-white flex flex-row">
-      <div className="p-2 left-0">
+    <div className={`sticky top-0 z-[50] w-full h-fit bg-white flex ${isOpen && !isDesktop ? "flex-col" : "flex-row"}`}>
+      <div className={`p-2 left-0 ${!isDesktop && "flex flex-row w-full"}`}>
         <LocalLink href={"/"}>
           <Logo />
         </LocalLink>
-      </div>
-      <div className="flex flex-row gap-10 w-full items-center justify-end">
-        {localLinkArr.map((item) => (
-          <LocalLink
-            key={item.pageName}
-            href={item.link}
-            className="text-lg font-DMSans-Bold"
+
+        {!isDesktop && 
+          <div 
+            className="flex flex-row w-full items-center justify-end pr-2"
+            onClick={() => setOpen((prev) => !prev)}
           >
-            {item.pageName}
-          </LocalLink>
-        ))}
-        <div className="flex flex-row gap-2 items-center justify-end pr-10">
-          <LocalLink href={"/sponsor-us"}>
-            <Button textColor="white" text="Sponsor Us" size="medium" />
-          </LocalLink>
-          <Button 
-            textColor="white" 
-            text="Apply" 
-            color="firecrackerRedLight" 
-            size="medium"
-            onClick={() => alert("2026 Applications haven't opened yet!")}
-          />
-        </div>
+            <RxHamburgerMenu size={36} color="black" />
+          </div>
+        }
       </div>
+
+      {isOpen && !isDesktop &&
+        <div className="flex flex-col gap-2 w-full items-start p-2 pl-4">
+          {localLinkArr.map((item) => (
+            <LocalLink
+              key={item.pageName}
+              href={item.link}
+              className="text-lg font-DMSans-Bold"
+            >
+              {item.pageName}
+            </LocalLink>
+          ))}
+          <div className="flex flex-col gap-2 items-start">
+            <LocalLink href={"/sponsor-us"}>
+              <Button textColor="white" text="Sponsor Us" size="medium" />
+            </LocalLink>
+            <Button
+              textColor="white"
+              text="Apply"
+              color="firecrackerRedLight"
+              size="medium"
+              onClick={() => alert("2026 Applications haven't opened yet!")}
+            />
+          </div>
+        </div>
+      }
+
+      {isDesktop && 
+        <div className="flex flex-row gap-10 w-full items-center justify-end">
+          {localLinkArr.map((item) => (
+            <LocalLink
+              key={item.pageName}
+              href={item.link}
+              className="text-lg font-DMSans-Bold"
+            >
+              {item.pageName}
+            </LocalLink>
+          ))}
+          <div className="flex flex-row gap-2 items-center justify-end pr-10">
+            <LocalLink href={"/sponsor-us"}>
+              <Button textColor="white" text="Sponsor Us" size="medium" />
+            </LocalLink>
+            <Button
+              textColor="white"
+              text="Apply"
+              color="firecrackerRedLight"
+              size="medium"
+              onClick={() => alert("2026 Applications haven't opened yet!")}
+            />
+          </div>
+        </div>
+      }
     </div>
   );
 };
