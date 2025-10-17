790985df3046cfb435fb4aff455e6add5e575ff2
adityapat24 <appathak46@gmail.com>
2025-10-16 21:05:13 -0400

    format

--- apps/main/src/app/lib/Assets/AboutLandingAssets/background.tsx
+++ apps/main/src/app/lib/Assets/AboutLandingAssets/background.tsx
@@ -2,11 +2,15 @@ import React from "react";
 
 const Background: React.FC = () => {
   return (
-    <svg width="1200" height="587" viewBox="0 0 1200 587" fill="none" xmlns="http://www.w3.org/2000/svg">
-<rect width="1200" height="587" fill="#B8C952"/>
-</svg>
-
-
+    <svg
+      width="1200"
+      height="587"
+      viewBox="0 0 1200 587"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width="1200" height="587" fill="#B8C952" />
+    </svg>
   );
 };
 export default Background;
