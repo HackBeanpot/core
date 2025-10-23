21a7d52fc5c3bdf6c7ccb9d0f8b686c1e6dd348c
Khushi Khan <khushikhan0301@gmail.com>
2025-10-22 21:50:32 -0400

    uploaded all the stat balloons as svg

--- apps/main/src/app/sponsor-us/page.tsx
+++ apps/main/src/app/sponsor-us/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import React from "react";
-import TicketBooth from "../lib/Assets/SVG/SponsorUsAssets/TicketBooth";
-import Ticket from "../lib/Assets/SVG/SponsorUsAssets/Ticket";
-import Button from "@repo/ui/Button";
-import useDevice from "@repo/util/hooks/useDevice";
+// import TicketBooth from "../lib/Assets/SVG/SponsorUsAssets/TicketBooth";
+// import Ticket from "../lib/Assets/SVG/SponsorUsAssets/Ticket";
+// import Button from "@repo/ui/Button";
+// import useDevice from "@repo/util/hooks/useDevice";
 import SponsorUsBenefitCardComp from "../lib/Components/SponsorUsComponents/SponsorUsBenefitCardComp";
 import SponsorTicketBoothComp from "../lib/Components/SponsorComponents/SponsorTicketBoothComp.tsx";
 
