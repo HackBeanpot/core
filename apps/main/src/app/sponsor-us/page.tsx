8ca9a9737bbe2c9d03654d6736886335383e682a
Khushi Khan <khushikhan0301@gmail.com>
2025-10-22 22:07:03 -0400

    sponsor us component starter code and cloud background

--- apps/main/src/app/sponsor-us/page.tsx
+++ apps/main/src/app/sponsor-us/page.tsx
@@ -5,17 +5,17 @@ import React from "react";
 // import Ticket from "../lib/Assets/SVG/SponsorUsAssets/Ticket";
 // import Button from "@repo/ui/Button";
 // import useDevice from "@repo/util/hooks/useDevice";
-import SponsorUsBenefitCardComp from "../lib/Components/SponsorUsComponents/SponsorUsBenefitCardComp";
-import SponsorTicketBoothComp from "../lib/Components/SponsorComponents/SponsorTicketBoothComp.tsx";
+// import SponsorUsBenefitCardComp from "../lib/Components/SponsorUsComponents/SponsorUsBenefitCardComp";
+// import SponsorTicketBoothComp from "../lib/Components/SponsorComponents/SponsorTicketBoothComp.tsx";
 
 const SponsorUsPage = () => {
   return (
     <div className="relative z-10 w-full">
-      <SponsorTicketBoothComp />
+      {/* <SponsorTicketBoothComp />
       <SponsorUsBenefitCardComp
         title="Market your company and product"
         content="Market your product and receive feedback by sponsoring a company-specific prize, hosting a workshop, giving a product demo, or mentoring hackers that are eager to hear your advice and implement your technologies for their projects."
-      />
+      /> */}
     </div>
   );
 };
