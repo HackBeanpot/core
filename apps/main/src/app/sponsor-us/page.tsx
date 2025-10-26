48e1da7b08c534cb6628fbd5caa9d1074fb95939
adityapat24 <appathak46@gmail.com>
2025-10-26 14:58:57 -0400

    comment out code

--- apps/main/src/app/sponsor-us/page.tsx
+++ apps/main/src/app/sponsor-us/page.tsx
@@ -5,7 +5,7 @@ import React from "react";
 // import Ticket from "../lib/Assets/SVG/SponsorUsAssets/Ticket";
 // import Button from "@repo/ui/Button";
 // import useDevice from "@repo/util/hooks/useDevice";
-import SponsorUsBenefitCardComp from "../lib/Components/SponsorUsComponents/SponsorUsBenefitCardComp";
+// import SponsorUsBenefitCardComp from "../lib/Components/SponsorUsComponents/SponsorUsBenefitCardComp";
 import SponsorTop from "./components/SponsorTop.tsx";
 
 const SponsorUsPage = () => {
@@ -15,12 +15,14 @@ const SponsorUsPage = () => {
         <SponsorTop />
       </div>
 
-      <div className="absolute top-[155%] z-10 w-full">
-        <SponsorUsBenefitCardComp
-          title="Market your company and product"
-          content="Market your product and receive feedback by sponsoring a company-specific prize, hosting a workshop, giving a product demo, or mentoring hackers that are eager to hear your advice and implement your technologies for their projects."
-        />
-      </div>
+      {/*
+<div className="absolute top-[155%] z-10 w-full">
+  <SponsorUsBenefitCardComp
+    title="Market your company and product"
+    content="Market your product and receive feedback by sponsoring a company-specific prize, hosting a workshop, giving a product demo, or mentoring hackers that are eager to hear your advice and implement your technologies for their projects."
+  />
+</div>
+*/}
     </div>
   );
 };
