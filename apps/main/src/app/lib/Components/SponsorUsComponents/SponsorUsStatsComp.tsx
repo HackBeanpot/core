56920b9dd2912201b7945a41ee91910f63e2b04a
Khushi Khan <khushikhan0301@gmail.com>
2025-10-26 00:11:37 -0400

    background assets reuploaded, background assembled

--- apps/main/src/app/lib/Components/SponsorUsComponents/SponsorUsStatsComp.tsx
+++ apps/main/src/app/lib/Components/SponsorUsComponents/SponsorUsStatsComp.tsx
@@ -1,10 +1,18 @@
 "use client";
 
 import React from "react";
+import CloudBackground from "../../Assets/SVG/SponsorUsAssets/CloudBackground";
+import TopClouds from "../../Assets/SVG/SponsorUsAssets/TopClouds";
+// import RibbonTitle from "@repo/ui/RibbonTitle";
 
 export default function SponsorUsBenefitCardComp() {
   return (
-    <div className="">
+    <div className="relative aspect-square w-full overflow-visible">
+      <TopClouds className="absolute top-0 z-10 w-[100vw]" />
+      <CloudBackground 
+        className="absolute bottom-0 w-[100vw] h-auto z-0"
+        preserveAspectRatio="xMidYMid meet"
+      />
     </div>
   );
 }
