7fc14a00895f93fa8d56b9fc36426c48e0f75aa2
adityapat24 <appathak46@gmail.com>
2025-10-26 22:37:46 -0400

    rounded edges

--- apps/main/src/app/sponsor-us/components/BoxWithText.tsx
+++ apps/main/src/app/sponsor-us/components/BoxWithText.tsx
@@ -13,7 +13,7 @@ const BoxWithText = () => {
       </div>
 
       <div className="mobile:scale-[0.90]">
-        <div className="relative bg-[#F88E78] outline font-['DMSans-Regular'] outline-[20px] outline-[#F88E78] drop-shadow-[4px_4px_0_#CC322D] max-w-3xl mx-auto">
+        <div className="relative bg-[#F88E78] rounded-sm outline font-['DMSans-Regular'] outline-[20px] outline-[#F88E78] drop-shadow-[4px_4px_0_#CC322D] max-w-3xl mx-auto">
           <div className="bg-[#C33A2E] rounded-xl shadow-[inset_-10px_10px_0_rgba(0,0,0,0.25)] p-8">
             <p className="text-white text-lg leading-relaxed">
               Sponsors empower HackBeanpot in celebrating innovation, diversity,
