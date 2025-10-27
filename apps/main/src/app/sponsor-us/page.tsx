114cf8f9f99262eb09a78eb278afa01dcf1a96dc
hackbeanbot <dev@hackbeanpot.com>
2025-10-27 16:26:35 -0400

    Update page.tsx

--- apps/main/src/app/sponsor-us/page.tsx
+++ apps/main/src/app/sponsor-us/page.tsx
@@ -3,15 +3,16 @@
 import React from "react";
 import SponsorUsBenefitCardComp from "../lib/Components/SponsorUsComponents/SponsorUsBenefitCardComp";
 import SponsorTicketBoothComp from "../lib/Components/SponsorComponents/SponsorTicketBoothComp.tsx";
+import SponsorUsStatsComp from "../lib/SponsorComponents/SponsorUsStatsComp";
 
 const SponsorUsPage = () => {
   return (
     <div className="relative z-10 w-full">
-      {/* <SponsorTicketBoothComp />
+      <SponsorTicketBoothComp />
       <SponsorUsBenefitCardComp
         title="Market your company and product"
         content="Market your product and receive feedback by sponsoring a company-specific prize, hosting a workshop, giving a product demo, or mentoring hackers that are eager to hear your advice and implement your technologies for their projects."
-      /> */}
+      /> 
       <SponsorUsStatsComp />
     </div>
   );
