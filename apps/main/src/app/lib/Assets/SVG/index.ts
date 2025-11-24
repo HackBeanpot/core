5a113fc2d3f37529f1f384af83f9c56889f84700
jjams <jjams0915@gmail.com>
2025-11-24 01:01:01 -0500

    + implement section

--- apps/main/src/app/lib/Assets/SVG/index.ts
+++ apps/main/src/app/lib/Assets/SVG/index.ts
@@ -3,3 +3,8 @@ export { default as ProjectStarIcon } from "./StarIcon";
 export { default as VolunteeringBackground } from "./VolunteeringAssets/VolunteeringBackground";
 export { default as ImpactSign } from "./SponsorUsAssets/ImpactSign";
 export { default as HeroBackground } from "./SponsorUsAssets/HeroBackground";
+// testimonial svgs
+export { default as TestimonialLeftBush } from "./TestimonialAssets/TestimonialLeftBush.tsx";
+export { default as TestimonialRightBush } from "./TestimonialAssets/TestimonialRightBush.tsx";
+export { default as TestimonialBottomSquiggle } from "./TestimonialAssets/TestimonialsBottomSquiggle.tsx";
+export { default as TrainTracks } from "./TestimonialAssets/TrainTracks.tsx";
