dae5e0f1113e9f3118dd27d45f5622d454113fff
Khushi Khan <khushikhan0301@gmail.com>
2025-12-14 02:41:52 -0500

    moved footer, updated package.json, improved styling of navbar, created navbarbase

--- apps/live/src/app/lib/Components/NavBar.tsx
+++ apps/live/src/app/lib/Components/NavBar.tsx
@@ -1,106 +1,23 @@
 "use client";
 
-// Imports
-import React, { useState } from "react";
-import Link from "next/link";
-
-// Icons
-import HomeIcon from "@repo/ui/Icons/HomeIcon";
-import MenuIcon from "../Assets/SVG/MenuIcon";
-import CloseIcon from "../Assets/SVG/CloseIcon";
-
-// Hooks
-import useIsMobile from "@repo/util/hooks/useIsMobile";
+import React from "react";
+import NavBarBase from "@repo/ui/NavBarBase";
 
 const NavBar = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const isMobile = useIsMobile();
-
   return (
-    <>
-      <div className="sticky top-0 z-[31] w-full">
-        <div className="bg-white flex flex-row w-full h-full justify-between align-middle drop-shadow-xl pr-[3vw]">
-          {/* TODO: Figure out scrolling to top smoothly*/}
-          <Link href={"/"} className={"px-0"}>
-            <HomeIcon />
-          </Link>
-
-          {isMobile ? (
-            <button
-              className="flex items-center justify-center p-2 text-black hover:scale-110"
-              onClick={() => setIsOpen(!isOpen)}
-            >
-              <MenuIcon />
-            </button>
-          ) : (
-            <div className="flex flex-row align-middle justify-end">
-              <Link
-                href={"#schedule"}
-                className="text-[clamp(1rem, 1.3vw, 4rem)] flex items-center px-[1.2vw] hover:scale-105 transition-transform"
-              >
-                Schedule
-              </Link>
-              <Link
-                href={"#mentors"}
-                className="text-[clamp(1rem,1.3vw, 4rem)] flex items-center px-[1.2vw] hover:scale-105 transition-transform"
-              >
-                Mentors
-              </Link>
-              <Link
-                href={"#resources"}
-                className="text-[clamp(1rem, 1.3vw, 4rem)] flex items-center px-[1.2vw] hover:scale-105 transition-transform"
-              >
-                Resources
-              </Link>
-              <Link
-                href={"#team"}
-                className="text-[clamp(1rem, 1.3vw, 4rem)] flex items-center px-[1.2vw] hover:scale-105 transition-transform"
-              >
-                Team
-              </Link>
-            </div>
-          )}
-        </div>
-
-        {isOpen && (
-          <div
-            className={`fixed top-0 right-0 h-full w-64 bg-white shadow-lg p-6 flex flex-col z-40`}
-          >
-            <button className="self-end p-3" onClick={() => setIsOpen(false)}>
-              <CloseIcon />
-            </button>
-            <Link
-              href={"#schedule"}
-              className="text-xl py-2 font-GT-Walsheim-Regular hover:bg-beige px-3 rounded-md cursor-pointer"
-              onClick={() => setIsOpen(false)}
-            >
-              Schedule
-            </Link>
-            <Link
-              href={"#mentors"}
-              className="text-xl py-2 font-GT-Walsheim-Regular hover:bg-beige px-3 rounded-md cursor-pointer"
-              onClick={() => setIsOpen(false)}
-            >
-              Mentors
-            </Link>
-            <Link
-              href={"#resources"}
-              className="text-xl py-2 font-GT-Walsheim-Regular hover:bg-beige px-3 rounded-md cursor-pointer"
-              onClick={() => setIsOpen(false)}
-            >
-              Resources
-            </Link>
-            <Link
-              href={"#team"}
-              className="text-xl py-2 font-GT-Walsheim-Regular hover:bg-beige px-3 rounded-md cursor-pointer"
-              onClick={() => setIsOpen(false)}
-            >
-              Team
-            </Link>
-          </div>
-        )}
-      </div>
-    </>
+    <NavBarBase
+      links={[
+        { pageName: "Resources", link: "#resources" },
+        { pageName: "Schedule", link: "#schedule" },
+        { pageName: "Guest Speakers", link: "#keynote" },
+        { pageName: "Mentors", link: "#mentors" },
+        { pageName: "Team", link: "#team" },
+      ]}
+      buttonLinks={[
+        { pageName: "Judging", link: "#judging", buttonColor: "starlightBlue" },
+      ]}
+      dropdownBgColor="bg-carouselCreamDark" // TODO NAVBAR: update the background color of the navbar dropdown once design gives u access
+    />
   );
 };
 
--- apps/live/src/app/page.tsx
+++ apps/live/src/app/page.tsx
@@ -6,7 +6,7 @@ import OurTeamBackground from "./Sections/OurTeamBackground";
 import HitTheRoad from "./Sections/HitTheRoad";
 import Resources from "./Sections/Resources";
 import NavBar from "./lib/Components/NavBar";
-import Footer from "./lib/Components/Footer";
+import Footer from "@repo/ui/Footer";
 import ComingUp from "./Sections/ComingUp/ComingUp";
 import OurTeam from "./Sections/OurTeam";
 import Keynote from "./Sections/Keynote";
