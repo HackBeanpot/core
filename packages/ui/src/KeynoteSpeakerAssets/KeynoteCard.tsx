21ad3c7920d816976439c38d2f0ce1e2f33623f0
roslyn-maloney <maloney.r@northeastern.edu>
2026-01-16 20:30:37 -0500

    added param for liveSite that toggles icons

--- packages/ui/src/KeynoteSpeakerAssets/KeynoteCard.tsx
+++ packages/ui/src/KeynoteSpeakerAssets/KeynoteCard.tsx
@@ -1,16 +1,24 @@
 import * as React from "react";
 import { SVGProps } from "react";
+import TimeIcon from "../TimeIcon";
+import LocationIcon from "../LocationIcon";
 
 interface KeynoteCardProps extends SVGProps<SVGSVGElement> {
   header: string;
   text: string;
   bio: string;
+  isLive: boolean;
+  location: string;
+  time: string;
 }
 
 const KeynoteCard: React.FC<KeynoteCardProps> = ({
   header,
   text,
   bio,
+  isLive,
+  location,
+  time,
   ...props
 }) => (
   <div>
@@ -22,6 +30,20 @@ const KeynoteCard: React.FC<KeynoteCardProps> = ({
         <h3 className="font-NeulisNeue-Bold text-xl text-firecrackerRed">
           {bio}
         </h3>
+        {isLive && (
+          <div className="flex items-center gap-4 text-charcoalFogDark">
+            <div className="flex items-center gap-1">
+              <LocationIcon />
+              <span>{location}</span>
+            </div>
+
+            <div className="flex items-center gap-1">
+              <TimeIcon />
+              <span>{time}</span>
+            </div>
+          </div>
+        )}
+
         <p className="text-charcoalFogDark mobile:text-s font-DMSans-Regular ">
           {text}
         </p>
--- packages/ui/src/KeynoteSpeakerSection.tsx
+++ packages/ui/src/KeynoteSpeakerSection.tsx
@@ -8,18 +8,23 @@ import KeynoteSpeakerPhoto from "./KeynoteSpeakerAssets/KeynoteSpeakerPhoto";
 import RibbonTitle from "@repo/ui/RibbonTitle";
 import KeynoteTopSquiggle from "./KeynoteSpeakerAssets/KeynoteTopSquiggle";
 
+
 export default function Keynote(): React.ReactNode {
   const { isMobile } = useDevice();
   const sueData = {
     header: "Sue Harnett",
     bio: "CEO of Rewriting the Code",
     text: "Sue Harnett is the Founder & CEO of Rewriting the Code, the nonprofit she launched in 2017 that has grown into a global community of 38,000+ university and early-career women in tech, offering mentorship, education, and career-readiness programs. She will be speaking about women in technology, and the value of community, teamwork, and thoughtful leadership!",
+    location: "Zone A",
+    time: "Friday, 1pm",
   };
 
   const jessicaData = {
     header: "Jessica Cao",
     bio: "Stanford CS '26 / Founder of World37",
     text: "Jessica Cao is an undergraduate studying Computer Science at Stanford University, and the founder of World37, an AI-powered storytelling platform where players can engage with, create, and share their own unique stories. She will be speaking about the art of pitching a technical project!",
+    location: "Zone A",
+    time: "Friday, 1pm",
   };
 
   return (
@@ -66,6 +71,9 @@ export default function Keynote(): React.ReactNode {
             header={sueData.header}
             bio={sueData.bio}
             text={sueData.text}
+            isLive={false}
+            location={sueData.location}
+            time={sueData.time}
           />
         </div>
       </div>
@@ -94,6 +102,9 @@ export default function Keynote(): React.ReactNode {
             header={jessicaData.header}
             bio={jessicaData.bio}
             text={jessicaData.text}
+            isLive={false}
+            location={jessicaData.location}
+            time={jessicaData.time}
           />
         </div>
       </div>
