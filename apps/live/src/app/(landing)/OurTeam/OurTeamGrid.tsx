785c219269a81ac63bbb2f624c4ff1e817a13eab
Khushi Khan <khushikhan0301@gmail.com>
2026-01-26 02:48:44 -0500

    styling for all screen sizes

--- apps/live/src/app/(landing)/OurTeam/OurTeamGrid.tsx
+++ apps/live/src/app/(landing)/OurTeam/OurTeamGrid.tsx
@@ -1,9 +1,12 @@
 "use client";
 
 import React, { useState } from "react";
-import Image from "next/image";
+import Icon from "@repo/ui/Icons/MemberIcon";
+import clsx from "clsx";
+import useDevice from "@util/hooks/useDevice";
 
 const TeamTable = () => {
+  const { isDesktop, isMobile } = useDevice();
   const teams = {
     Directors: [
       {
@@ -15,6 +18,38 @@ const TeamTable = () => {
         name: "Rachel Pao",
         src: "/headshots/directors/Rachel.png",
         url: "https://www.linkedin.com/in/rachelpaocyber/",
+      },
+    ],
+    Design: [
+      {
+        name: "Zahra Wibisana",
+        src: "/headshots/design/Zahra.png",
+        url: "https://www.linkedin.com/in/zahra-wibisana-0b0bb2222/",
+      },
+      {
+        name: "Isabella Borda",
+        src: "/headshots/design/Isabella.png",
+        url: "https://www.linkedin.com/in/isabella-borda-03537b306/",
+      },
+      {
+        name: "Annabelle Chung",
+        src: "/headshots/design/Annabelle.png",
+        url: "https://www.linkedin.com/in/annabelle-chung-/",
+      },
+      {
+        name: "Cole Abrams",
+        src: "/headshots/design/Cole.png",
+        url: "https://www.linkedin.com/in/coleabrams/",
+      },
+      {
+        name: "Lucy Liu",
+        src: "/headshots/design/Lucy.png",
+        url: "https://www.linkedin.com/in/xinninglucyliu/",
+      },
+      {
+        name: "Yurika Kan",
+        src: "/headshots/design/Yurika.png",
+        url: "https://www.linkedin.com/in/yurika-kan/",
       },
     ],
     Tech: [
@@ -69,39 +104,66 @@ const TeamTable = () => {
         url: "https://www.linkedin.com/in/roslyn-maloney-nustudent/",
       },
     ],
-    Design: [
+    Operations: [
       {
-        name: "Zahra Wibisana",
-        src: "/headshots/design/Zahra.png",
-        url: "https://www.linkedin.com/in/zahra-wibisana-0b0bb2222/",
+        name: "Amy Wang",
+        src: "/headshots/operations/Amy.png",
+        url: "https://www.linkedin.com/in/amy-wang-17b526248/",
       },
       {
-        name: "Isabella Borda",
-        src: "/headshots/design/Isabella.png",
-        url: "https://www.linkedin.com/in/isabella-borda-03537b306/",
+        name: "Zoe Gao",
+        src: "/headshots/operations/Zoe.png",
+        url: "https://www.linkedin.com/in/zoe-gao-khoury",
       },
       {
-        name: "Annabelle Chung",
-        src: "/headshots/design/Annabelle.png",
-        url: "https://www.linkedin.com/in/annabelle-chung-/",
+        name: "Nicole Ni",
+        src: "/headshots/operations/Nicole.png",
+        url: "https://www.linkedin.com/in/nicoleni22",
       },
       {
-        name: "Cole Abrams",
-        src: "/headshots/design/Cole.png",
-        url: "https://www.linkedin.com/in/coleabrams/",
+        name: "Fanta Kébé",
+        src: "/headshots/operations/Fanta.png",
+        url: "https://www.linkedin.com/in/fantaa-kebe?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BSKzRhbsvTIaBt8MSstJu3w%3D%3D",
       },
       {
-        name: "Lucy Liu",
-        src: "/headshots/design/Lucy.png",
-        url: "https://www.linkedin.com/in/xinninglucyliu/",
-      },
-      {
-        name: "Yurika Kan",
-        src: "/headshots/design/Yurika.png",
-        url: "https://www.linkedin.com/in/yurika-kan/",
+        name: "Nodoka Shibasaki",
+        src: "/headshots/operations/Nodoka.png",
+        url: "https://www.linkedin.com/in/nodoka2027/",
       },
     ],
-    "Socials&Outreach": [
+    Sponsorship: [
+      {
+        name: "Jesse James",
+        src: "/headshots/sponsorship/Jesse.png",
+        url: "https://www.linkedin.com/in/jessejamescs/",
+      },
+      {
+        name: "Sammi Chen",
+        src: "/headshots/sponsorship/Sammi.png",
+        url: "https://www.linkedin.com/in/chensammi/",
+      },
+      {
+        name: "Aaryan Jain",
+        src: "/headshots/sponsorship/Aaryan.png",
+        url: "https://www.linkedin.com/aaryanja",
+      },
+      {
+        name: "Johny Sargent",
+        src: "/headshots/sponsorship/Johny.png",
+        url: "https://www.linkedin.com/in/john--sargent/",
+      },
+      {
+        name: "Jolin Yang",
+        src: "/headshots/sponsorship/Jolin.png",
+        url: "https://www.linkedin.com/in/jolin-yang-ba8241246/",
+      },
+      {
+        name: "Livia Cutra",
+        src: "/headshots/sponsorship/Livia.png",
+        url: "http://www.linkedin.com/in/liviacutra",
+      },
+    ],
+    "S&O": [
       {
         name: "Megan Lai",
         src: "/headshots/socials/Megan.png",
@@ -138,65 +200,6 @@ const TeamTable = () => {
         url: "http://www.linkedin.com/in/susan-42-chen",
       },
     ],
-    Sponsorship: [
-      {
-        name: "Jesse James",
-        src: "/headshots/sponsorship/Jesse.png",
-        url: "https://www.linkedin.com/in/jessejamescs/",
-      },
-      {
-        name: "Sammi Chen",
-        src: "/headshots/sponsorship/Sammi.png",
-        url: "https://www.linkedin.com/in/chensammi/",
-      },
-      {
-        name: "Aaryan Jain",
-        src: "/headshots/sponsorship/Aaryan.png",
-        url: "https://www.linkedin.com/aaryanja",
-      },
-      {
-        name: "Johny Sargent",
-        src: "/headshots/sponsorship/Johny.png",
-        url: "https://www.linkedin.com/in/john--sargent/",
-      },
-      {
-        name: "Jolin Yang",
-        src: "/headshots/sponsorship/Jolin.png",
-        url: "https://www.linkedin.com/in/jolin-yang-ba8241246/",
-      },
-      {
-        name: "Livia Cutra",
-        src: "/headshots/sponsorship/Livia.png",
-        url: "http://www.linkedin.com/in/liviacutra",
-      },
-    ],
-    Operations: [
-      {
-        name: "Amy Wang",
-        src: "/headshots/operations/Amy.png",
-        url: "https://www.linkedin.com/in/amy-wang-17b526248/",
-      },
-      {
-        name: "Zoe Gao",
-        src: "/headshots/operations/Zoe.png",
-        url: "https://www.linkedin.com/in/zoe-gao-khoury",
-      },
-      {
-        name: "Nicole Ni",
-        src: "/headshots/operations/Nicole.png",
-        url: "https://www.linkedin.com/in/nicoleni22",
-      },
-      {
-        name: "Fanta Kébé",
-        src: "/headshots/operations/Fanta.png",
-        url: "https://www.linkedin.com/in/fantaa-kebe?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BSKzRhbsvTIaBt8MSstJu3w%3D%3D",
-      },
-      {
-        name: "Nodoka Shibasaki",
-        src: "/headshots/operations/Nodoka.png",
-        url: "https://www.linkedin.com/in/nodoka2027/",
-      },
-    ],
   };
   type teamKey = keyof typeof teams;
   const [currTeam, setCurrTeam] = useState<teamKey>("Tech");
@@ -205,54 +208,35 @@ const TeamTable = () => {
     setCurrTeam(team);
   };
 
+  const iconGridStyles = clsx(
+    "flex flex-wrap justify-center items-center mx-auto gap-6",
+    isDesktop && "w-3/4",
+    isMobile && "grid grid-cols-2"
+  );
+
   return (
-    <div className="py-4">
-      <div className="flex flex-row gap-4">
+    <div className="relative text-white flex flex-col gap-8">
+      <div className="flex flex-row gap-4 flex-wrap items-center justify-center">
         {Object.entries(teams).map(([teamName, team]) => (
           <button
             key={team.toString()}
             onClick={() => changeTeam(teamName as keyof typeof teams)}
-            className={`py-4 px-2 transition-transform duration-300 transform scale-100 hover:scale-[102%] ${
-              currTeam === teamName ? "font-bold" : "font-normal"
+            className={`py-2 px-3 transition-transform duration-300 transform scale-100 hover:scale-[102%] rounded-xl font-NeulisNeue-Bold text-[20px] ${
+              currTeam === teamName ? "bg-firecrackerRed" : "border border-white"
             }`}
           >
             {teamName.toString()}
           </button>
         ))}
       </div>
-      <div className="grid grid-cols-5 grid-rows-2 gap-2 md:grid-cols-2 md:grid-rows-5">
+      <div className={iconGridStyles}>
         {teams[currTeam].map((member, index) => (
           <a
             key={index}
             href={member.url}
             className="transition-transform scale-100 hover:scale-105"
           >
-            <div className="items-center p-2 rounded-lg ">
-              <div className="w-full pb-[100%] relative overflow-hidden rounded-lg">
-                <Image
-                  src={member.src}
-                  alt={member.src}
-                  fill
-                  className="absolute w-full h-full object-cover rounded-lg"
-                />
-              </div>
-              <div className="grid grid-cols-[20%_80%] items-center mt-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  x="0px"
-                  y="0px"
-                  width="100"
-                  height="100"
-                  viewBox="0 0 30 30"
-                  className="h-8 pr-[5rem]"
-                >
-                  <path d="M24,4H6C4.895,4,4,4.895,4,6v18c0,1.105,0.895,2,2,2h18c1.105,0,2-0.895,2-2V6C26,4.895,25.105,4,24,4z M10.954,22h-2.95 v-9.492h2.95V22z M9.449,11.151c-0.951,0-1.72-0.771-1.72-1.72c0-0.949,0.77-1.719,1.72-1.719c0.948,0,1.719,0.771,1.719,1.719 C11.168,10.38,10.397,11.151,9.449,11.151z M22.004,22h-2.948v-4.616c0-1.101-0.02-2.517-1.533-2.517 c-1.535,0-1.771,1.199-1.771,2.437V22h-2.948v-9.492h2.83v1.297h0.04c0.394-0.746,1.356-1.533,2.791-1.533 c2.987,0,3.539,1.966,3.539,4.522V22z"></path>
-                </svg>
-                <p className="text-sm font-medium truncate w-full place-self-start mt-1 ">
-                  {member.name}
-                </p>
-              </div>
-            </div>
+            <Icon src={member.src} name={member.name} isLive={false} isActive={false} textColor="white" />
           </a>
         ))}
       </div>
--- apps/live/src/app/(landing)/OurTeam/index.tsx
+++ apps/live/src/app/(landing)/OurTeam/index.tsx
@@ -2,22 +2,48 @@
 import React from "react";
 import OurTeamGrid from "./OurTeamGrid";
 import RibbonTitle from "@repo/ui/RibbonTitle";
+import clsx from "clsx";
+import useDevice from "@util/hooks/useDevice";
 
 export default function OurTeam(): React.ReactNode {
+  const { isDesktop, isTablet, isMobile } = useDevice();
+
+  const innerStyles = clsx(
+    "font-NeulisNeue-Regular flex flex-col gap-10 items-center justify-center",
+    isDesktop && "py-16 desktop:px-44 desktop-xl:px-80",
+    isTablet && "px-28",
+  );
+
+  const ribbonStyles = clsx(
+    isDesktop && "scale-100",
+    isTablet && "scale-75",
+    isMobile && "scale-[60%]"
+  );
+
+  const blurbStyles = clsx(
+    "flex flex-col bg-carouselCreamLight rounded-3xl gap-2 drop-shadow-[0_6px_0px_rgba(0,0,0,0.25)] p-8",
+    isDesktop && "w-3/4",
+  );
+
   return (
-    <div className="py-24 px-48 font-NeulisNeue-Regular flex flex-col gap-8 relative">
-      <RibbonTitle text={"OUR TEAM"} />
-      <div className="bg-carouselCreamLight rounded-xl p-8">
-        <h1 className="font-NeulisNeue-Bold text-firecrackerRed text-[24px]">
-          Need to reach out to a core member?
-        </h1>
-        <p className="font-DM-Sans-Regular">
-          Feel free to reach out to a core member via Discord if you have any
-          questions or concerts about event logistics, applying to be part of
-          the HBP core team, or more!
-        </p>
+    <div className="relative bg-mossGreenDark w-full h-full py-20">
+      <div className={innerStyles}>
+        <div className={ribbonStyles}>
+          <RibbonTitle text={"OUR TEAM"} />
+        </div>
+        <div className={blurbStyles}>
+          <h1 className="font-NeulisNeue-Bold text-firecrackerRed text-[24px]">
+            Need to reach out to a core member?
+          </h1>
+          <p className="font-DM-Sans-Regular">
+            Feel free to reach out to a core member via Discord if you have any
+            questions or concerts about event logistics, applying to be part of
+            the HBP core team, or more!
+          </p>
+        </div>
+        <OurTeamGrid />
       </div>
-      <OurTeamGrid />
     </div>
+    
   );
 }
