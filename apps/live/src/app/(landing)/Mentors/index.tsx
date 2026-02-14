b302cf9b68e97c8421d88dac50d29c0d51a36a92
Khushi Khan <khushikhan0301@gmail.com>
2026-02-13 22:02:49 -0500

    Mentor section is more compact now

--- apps/live/src/app/(landing)/Mentors/index.tsx
+++ apps/live/src/app/(landing)/Mentors/index.tsx
@@ -73,6 +73,7 @@ const MentorSection = () => {
         </div>
         {/* Mentor listing (renders when Airtable data available) */}
         <MentorsTable data={data} />
+        
       </div>
     );
   });
