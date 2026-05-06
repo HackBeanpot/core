a1fc27bb2892224f6a452d4ae477ffa0987df539
adityapat24 <appathak46@gmail.com>
2026-05-06 14:04:57 -0400

    initial app portal setup

---
+++ apps/app-portal/src/components/ui/sonner.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import type * as React from "react";
+import { Toaster as Sonner } from "sonner";
+
+type ToasterProps = React.ComponentProps<typeof Sonner>;
+
+const Toaster = ({ ...props }: ToasterProps) => {
+  return <Sonner className="toaster group" position="top-right" richColors {...props} />;
+};
+
+export { Toaster };
