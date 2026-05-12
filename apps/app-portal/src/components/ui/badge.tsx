30761088378cc346d36044ddc80f092107225858
Michael Zhang <zhang.michael1@northeastern.edu>
2026-05-12 23:07:07 +0800

    init new files commit

--- apps/app-portal/src/components/ui/badge.tsx
+++ apps/app-portal/src/components/ui/badge.tsx
@@ -3,24 +3,31 @@ import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "@/lib/utils";
 
-const badgeVariants = cva("inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors", {
-  variants: {
-    variant: {
-      default: "border-transparent bg-black text-white",
-      secondary: "border-transparent bg-neutral-100 text-neutral-900",
-      destructive: "border-transparent bg-red-600 text-white",
-      outline: "text-neutral-950",
+const badgeVariants = cva(
+  "inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors",
+  {
+    variants: {
+      variant: {
+        default: "border-transparent bg-black text-white",
+        secondary: "border-transparent bg-neutral-100 text-neutral-900",
+        destructive: "border-transparent bg-red-600 text-white",
+        outline: "text-neutral-950",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
     },
   },
-  defaultVariants: {
-    variant: "default",
-  },
-});
+);
 
-export interface BadgeProps extends React.HTMLAttributes<HTMLDivElement>, VariantProps<typeof badgeVariants> {}
+export interface BadgeProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof badgeVariants> {}
 
 function Badge({ className, variant, ...props }: BadgeProps) {
-  return <div className={cn(badgeVariants({ variant }), className)} {...props} />;
+  return (
+    <div className={cn(badgeVariants({ variant }), className)} {...props} />
+  );
 }
 
 export { Badge, badgeVariants };
