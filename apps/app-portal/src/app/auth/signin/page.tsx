66b52c2ee80688adb79de1e9e0527431681c3b90
Michael Zhang <zhang.michael1@northeastern.edu>
2026-06-04 14:35:54 -0400

    draft

--- apps/app-portal/src/app/auth/signin/page.tsx
+++ apps/app-portal/src/app/auth/signin/page.tsx
@@ -3,28 +3,36 @@ import React from "react";
 import Image from "next/image";
 import icon from "@/app/icon.ico";
 
-
 const gap = 4;
 
 function SignInForm() {
-  return <>
-    <div className="w-[100vw] h-[100vh] flex flex-col justify-center items-center">
-      {/*icon*/}
-      <Image src={icon} alt="HBP Logo" width={64} height={64}/>
-      {/*title*/}
-      <p className={`mt-${gap} text-black text-2xl`}>
-        HackBeanpot
-      </p>
-      {/*input*/}
-      <input placeholder="Email" className={`p-1 mt-${gap} border-2 rounded-md`}/>
-      {/*confirm*/}
-      <button className={`bg-starlightBlueLight text-white p-1 px-5 mt-${gap} border-2 rounded-md`}>Sign in with email</button>
-    </div>
-  </>
+  return (
+    <>
+      <div className="w-[100vw] h-[100vh] flex flex-col justify-center items-center">
+        {/*icon*/}
+        <Image src={icon} alt="HBP Logo" width={64} height={64} />
+        {/*title*/}
+        <p className={`mt-${gap} text-black text-2xl`}>HackBeanpot</p>
+        {/*input*/}
+        <input
+          placeholder="Email"
+          className={`p-1 mt-${gap} border-2 rounded-md`}
+        />
+        {/*confirm*/}
+        <button
+          className={`bg-starlightBlueLight text-white p-1 px-5 mt-${gap} border-2 rounded-md`}
+        >
+          Sign in with email
+        </button>
+      </div>
+    </>
+  );
 }
 
 export default function Page(): JSX.Element {
-  return <>
-    <SignInForm/>
-  </>;
+  return (
+    <>
+      <SignInForm />
+    </>
+  );
 }
